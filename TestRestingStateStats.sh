5b8d4dc05c5c4ed929d85bb3acd9cadf173dbad2
Timothy B. Brown <tbbrown@wustl.edu>
2015-07-16 13:15:07 -0500

    Fixed TestRestingStateStats.sh to use --server option instead of --host

--- TestRestingStateStats.sh
+++ TestRestingStateStats.sh
@@ -47,7 +47,7 @@ touch ${script_file_to_run}
 echo "/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/RestingStateStats.XNAT.sh \\" >> ${script_file_to_run}
 echo "  --user=\"${token_username}\" \\" >> ${script_file_to_run}
 echo "  --password=\"${token_password}\" \\" >> ${script_file_to_run}
-echo "  --host=\"${server}\" \\" >> ${script_file_to_run}
+echo "  --server=\"${server}\" \\" >> ${script_file_to_run}
 echo "  --project=\"${project}\" \\" >> ${script_file_to_run}
 echo "  --subject=\"${subject}\" \\" >> ${script_file_to_run}
 echo "  --session=\"${session}\" \\" >> ${script_file_to_run}
