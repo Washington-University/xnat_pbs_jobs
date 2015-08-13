d87fb4b0637d2d5052b28f40ea8c4044b1bbb8c3
Timothy B. Brown <tbbrown@wustl.edu>
2015-08-13 11:58:18 -0500

    Created versions of GetHcpDataUtils functions that created symbolic links instead of copying data

--- GetHcpDataUtils/GetHcpDataUtils.sh
+++ GetHcpDataUtils/GetHcpDataUtils.sh
@@ -1,10 +1,55 @@
 
+link_hcp_struct_preproc_data()
+{
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hcpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local to_study_dir=${5}
+
+	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
+	local DATABASE_RESOURCES_ROOT="RESOURCES"
+
+	echo ""
+	echo "----------" `date` "----------"
+    echo "Linking HCP Structurally Preprocessed data from archive"
+    echo " Archive: ${archive}"
+    echo " Project: ${project}"
+    echo " Subject: ${subject}"
+    echo " Session: ${session}"
+    echo " To Study Directory: ${to_study_dir}"
+
+	pushd ${to_study_dir}
+	mkdir -p ${subject}
+
+    local link_from=""
+    link_from+="${archive}"
+    link_from+="/${project}"
+    link_from+="/${DATABASE_ARCHIVE_PROJECT_ROOT}"
+    link_from+="/${session}"
+    link_from+="/${DATABASE_RESOURCES_ROOT}"
+    link_from+="/Structural_preproc/*"
+
+    local link_to=""
+    link_to="${to_study_dir}/${subject}"
+
+	local lndir_cmd=""
+	lndir_cmd="lndir ${link_from} ${link_to}"
+	echo "lndir_cmd: ${lndir_cmd}"
+
+	echo "----------" `date` "----------"
+	echo ""
+    ${lndir_cmd}
+
+    popd
+}
+
 get_hcp_struct_preproc_data()
 {
-    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hcpdb/archive                                                                                                                                      
-    local project=${2} # e.g. HCP_500                                                                                                                                                                        
-    local subject=${3} # e.g. 100307                                                                                                                                                                         
-    local session=${4} # e.g. 100307_3T                                                                                                                                                                      
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hcpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
     local to_study_dir=${5}
 
 	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
@@ -43,13 +88,60 @@ get_hcp_struct_preproc_data()
     popd
 }
 
+link_hcp_func_preproc_data()
+{
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hcpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
+    local to_study_dir=${6}
+
+	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
+	local DATABASE_RESOURCES_ROOT="RESOURCES"
+
+	echo ""
+	echo "----------" `date` "----------"
+    echo "Linking HCP Functionally Preprocessed data from archive"
+    echo " Archive: ${archive}"
+    echo " Project: ${project}"
+    echo " Subject: ${subject}"
+    echo " Session: ${session}"
+    echo " Scan: ${scan}"
+    echo " To Study Directory: ${to_study_dir}"
+
+    pushd ${to_study_dir}
+    mkdir -p ${subject}
+
+    local link_from=""
+    link_from+="${archive}"
+    link_from+="/${project}"
+    link_from+="/${DATABASE_ARCHIVE_PROJECT_ROOT}"
+    link_from+="/${session}"
+    link_from+="/${DATABASE_RESOURCES_ROOT}"
+    link_from+="/${scan}_preproc/*"
+
+    local link_to=""
+    link_to="${to_study_dir}/${subject}"
+
+    local lndir_cmd=""
+    lndir_cmd="lndir ${link_from} ${link_to}"
+    echo "lndir_cmd: ${lndir_cmd}"
+
+	echo "----------" `date` "----------"
+	echo ""
+    ${lndir_cmd}
+
+    popd
+}
+
 get_hcp_func_preproc_data()
 {
-    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hcpdb/archive                                                                                                                                      
-    local project=${2} # e.g. HCP_500                                                                                                                                                                        
-    local subject=${3} # e.g. 100307                                                                                                                                                                         
-    local session=${4} # e.g. 100307_3T                                                                                                                                                                      
-    local scan=${5}    # e.g. rfMRI_REST1_RL                                                                                                                                                                 
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hcpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
     local to_study_dir=${6}
 
 	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
@@ -89,13 +181,59 @@ get_hcp_func_preproc_data()
     popd
 }
 
+link_hcp_fix_proc_data()
+{
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
+    local to_study_dir=${6}
+
+	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
+	local DATABASE_RESOURCES_ROOT="RESOURCES"
+
+	echo ""
+	echo "----------" `date` "----------"
+    echo "Linking HCP FIX Processed data from archive"
+    echo " Archive: ${archive}"
+    echo " Project: ${project}"
+    echo " Subject: ${subject}"
+    echo " Session: ${session}"
+    echo " Scan: ${scan}"
+    echo " To Study Directory: ${to_study_dir}"
+
+    pushd ${to_study_dir}
+    mkdir -p ${subject}/MNINonLinear/Results
+
+    local link_from=""
+    link_from+="${archive}"
+    link_from+="/${project}"
+    link_from+="/${DATABASE_ARCHIVE_PROJECT_ROOT}"
+    link_from+="/${session}"
+    link_from+="/${DATABASE_RESOURCES_ROOT}"
+    link_from+="/${scan}_FIX/*"
+
+    local link_to=""
+    link_to="${to_study_dir}/${subject}/MNINonLinear/Results"
+
+    local lndir_cmd=""
+    lndir_cmd="lndir ${link_from} ${link_to}"
+    echo "lndir_cmd: ${lndir_cmd}"
+	echo "----------" `date` "----------"
+	echo ""
+    ${lndir_cmd}
+
+    popd
+}
+
 get_hcp_fix_proc_data()
 {
-    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive                                                                                                                                      
-    local project=${2} # e.g. HCP_500                                                                                                                                                                        
-    local subject=${3} # e.g. 100307                                                                                                                                                                         
-    local session=${4} # e.g. 100307_3T                                                                                                                                                                      
-    local scan=${5}    # e.g. rfMRI_REST1_RL                                                                                                                                                                 
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
     local to_study_dir=${6}
 
 	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
@@ -135,13 +273,13 @@ get_hcp_fix_proc_data()
     popd
 }
 
-get_hcp_postfix_data()
+link_hcp_postfix_data()
 {
-    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive                                                                                                                                      
-    local project=${2} # e.g. HCP_500                                                                                                                                                                        
-    local subject=${3} # e.g. 100307                                                                                                                                                                         
-    local session=${4} # e.g. 100307_3T                                                                                                                                                                      
-    local scan=${5}    # e.g. rfMRI_REST1_RL                                                                                                                                                                 
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
     local to_study_dir=${6}
 
 	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
@@ -149,7 +287,53 @@ get_hcp_postfix_data()
 
 	echo ""
 	echo "----------" `date` "----------"
-    echo "Copying HCP PosFix data from archive"
+    echo "Linking HCP PostFix data from archive"
+    echo " Archive: ${archive}"
+    echo " Project: ${project}"
+    echo " Subject: ${subject}"
+    echo " Session: ${session}"
+    echo " Scan: ${scan}"
+    echo " To Study Directory: ${to_study_dir}"
+
+    pushd ${to_study_dir}
+    mkdir -p ${subject}/MNINonLinear/Results/${scan}
+
+    local link_from=""
+    link_from+="${archive}"
+    link_from+="/${project}"
+    link_from+="/${DATABASE_ARCHIVE_PROJECT_ROOT}"
+    link_from+="/${session}"
+    link_from+="/${DATABASE_RESOURCES_ROOT}"
+    link_from+="/${scan}_PostFix/${subject}/MNINonLinear/Results/${scan}"
+
+    local link_to=""
+    link_to="${to_study_dir}/${subject}/MNINonLinear/Results"
+
+    local lndir_cmd=""
+    lndir_cmd="lndir ${link_from} ${link_to}"
+    echo "lndir_cmd: ${lndir_cmd}"
+	echo "----------" `date` "----------"
+	echo ""
+    ${lndir_cmd}
+
+    popd
+}
+
+get_hcp_postfix_data()
+{
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
+    local to_study_dir=${6}
+
+	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
+	local DATABASE_RESOURCES_ROOT="RESOURCES"
+
+	echo ""
+	echo "----------" `date` "----------"
+    echo "Copying HCP PostFix data from archive"
     echo " Archive: ${archive}"
     echo " Project: ${project}"
     echo " Subject: ${subject}"
@@ -181,13 +365,59 @@ get_hcp_postfix_data()
     popd
 }
 
+link_hcp_resting_state_stats_data()
+{
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
+    local to_study_dir=${6}
+
+	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
+	local DATABASE_RESOURCES_ROOT="RESOURCES"
+
+	echo ""
+	echo "----------" `date` "----------"
+    echo "Linking HCP Resting State Stats data from archive"
+    echo " Archive: ${archive}"
+    echo " Project: ${project}"
+    echo " Subject: ${subject}"
+    echo " Session: ${session}"
+    echo " Scan: ${scan}"
+    echo " To Study Directory: ${to_study_dir}"
+
+    pushd ${to_study_dir}
+    mkdir -p ${subject}/MNINonLinear/Results/${scan}
+
+    local link_from=""
+    link_from+="${archive}"
+    link_from+="/${project}"
+    link_from+="/${DATABASE_ARCHIVE_PROJECT_ROOT}"
+    link_from+="/${session}"
+    link_from+="/${DATABASE_RESOURCES_ROOT}"
+    link_from+="/${scan}_RSS/${subject}/MNINonLinear/Results/${scan}"
+
+    local link_to=""
+    link_to="${to_study_dir}/${subject}/MNINonLinear/Results"
+
+    local lndir_cmd=""
+    lndir_cmd="lndir ${link_from} ${link_to}"
+    echo "lndir_cmd: ${lndir_cmd}"
+	echo "----------" `date` "----------"
+	echo ""
+    ${lndir_cmd}
+
+    popd
+}
+
 get_hcp_resting_state_stats_data()
 {
-    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive                                                                                                                                      
-    local project=${2} # e.g. HCP_500                                                                                                                                                                        
-    local subject=${3} # e.g. 100307                                                                                                                                                                         
-    local session=${4} # e.g. 100307_3T                                                                                                                                                                      
-    local scan=${5}    # e.g. rfMRI_REST1_RL                                                                                                                                                                 
+    local archive=${1} # e.g. /data/hcpdb/archive or /HCP/hdpdb/archive
+    local project=${2} # e.g. HCP_500
+    local subject=${3} # e.g. 100307
+    local session=${4} # e.g. 100307_3T
+    local scan=${5}    # e.g. rfMRI_REST1_RL
     local to_study_dir=${6}
 
 	local DATABASE_ARCHIVE_PROJECT_ROOT="arc001"
