8499bf2b0bd21f51b58283d15dd7c399b0e33d41
Timothy B. Brown <tbbrown@wustl.edu>
2015-09-18 13:31:49 -0500

    Put all data generated by MSMRemoveGroupDrift in to project level resource

--- MSMRemoveGroupDrift/PutGroupDriftData.sh
+++ MSMRemoveGroupDrift/PutGroupDriftData.sh
@@ -3,20 +3,9 @@ set -e
 
 echo "Job started on `hostname` at `date`"
 
-# home directory for scripts to be sourced to set up the environment 
-SCRIPTS_HOME=/home/HCPpipeline/SCRIPTS
-echo "SCRIPTS_HOME: ${SCRIPTS_HOME}"
-
-# root directory of the XNAT database archive
-DATABASE_ARCHIVE_ROOT="/HCP/hcpdb/archive"
-echo "DATABASE_ARCHIVE_ROOT: ${DATABASE_ARCHIVE_ROOT}"
-
 # home directory for these XNAT PBS job scripts
 XNAT_PBS_JOBS_HOME=/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs
 echo "XNAT_PBS_JOBS_HOME: ${XNAT_PBS_JOBS_HOME}"
-
-# Load Function Libraries
-source ${XNAT_PBS_JOBS_HOME}/GetHcpDataUtils/GetHcpDataUtils.sh
 
 # Database Resource names and suffixes
 echo "Defining Database Resource Names and Suffixes"
@@ -26,9 +15,9 @@ source ${XNAT_PBS_JOBS_HOME}/GetHcpDataUtils/ResourceNamesAndSuffixes.sh
 usage()
 {
 	echo ""
-	echo "  Compute Group Registration Drift"
+	echo "  Put Group Drift Data in XNAT DB"
 	echo ""
-	echo "  Usage: MSMRemoveGroupDrift.XNAT.sh <options>"
+	echo "  Usage: PutGroupDriftData.sh <options>"
 	echo ""
 	echo "  To Be Written"
 	echo ""
@@ -44,7 +33,7 @@ get_options()
 	unset g_user
 	unset g_password
 	unset g_server
-	unset g_subject_info_file
+	unset g_project
 	unset g_working_dir
 
 	# parse arguments
@@ -72,8 +61,8 @@ get_options()
 				g_server=${argument/*=/""}
 				index=$(( index + 1 ))
 				;;
-			--subject-info-file=*)
-				g_subject_info_file=${argument/*=/""}
+			--project=*)
+				g_project=${argument/*=/""}
 				index=$(( index + 1 ))
 				;;
 			--working-dir=*)
@@ -114,11 +103,11 @@ get_options()
 		echo "g_server: ${g_server}"
 	fi
 
-	if [ -z "${g_subject_info_file}" ]; then
-		echo "ERROR: subject info file (--subject-info-file=) required"
+	if [ -z "${g_project}" ]; then
+		echo "ERROR: project (--project=) required"
 		error_count=$(( error_count + 1 ))
 	else
-		echo "g_subject_info_file: ${g_subject_info_file}"
+		echo "g_project: ${g_project}"
 	fi
 
 	if [ -z "${g_working_dir}" ]; then
@@ -136,94 +125,40 @@ get_options()
 	fi
 }
 
-get_subject_info_list()
-{
-	local file_name="${1}"
-	echo "Retrieving subject info list from: ${file_name}"
-	local subject_info_from_file=( $( cat ${file_name} ) )
-	g_subjects_info="`echo "${subject_info_from_file[@]}"`"
-	if [ -z "${g_subjects_info}" ]; then
-		echo "ERROR: No Subjects Specified" 
-		exit 1
-	fi
-}
-
-get_project()
-{
-	local subject_info="${1}"
-	local project=`echo ${subject_info} | cut -d ":" -f 1`
-	echo ${project}
-}
-
-get_subject()
-{
-	local subject_info="${1}"
-	local subject=`echo ${subject_info} | cut -d ":" -f 2`
-	echo ${subject}
-}
-
-get_session()
-{
-	local subject_info="${1}"
-	local session=`echo ${subject_info} | cut -d ":" -f 3`
-	echo ${session}
-}
-
-push_subject_files_to_db()
-{
-	local project
-	local subject
-	local session
-	local db_working_dir
-
-	for subject_info in ${g_subjects_info} ; do
-		project=`get_project ${subject_info}`
-		subject=`get_subject ${subject_info}`
-		session=`get_session ${subject_info}`
-		echo "Pushing data to project: ${project} for subject: ${subject} in session ${session}"
-
-		echo "-------------------------------------------------"
-		echo "Deleting previous resource"	
-		echo "-------------------------------------------------"
-		${XNAT_PBS_JOBS_HOME}/WorkingDirPut/DeleteResource.sh \
-			--user="${g_user}" \
-			--password="${g_password}" \
-			--server="${g_server}" \
-			--project="${project}" \
-			--subject="${subject}" \
-			--session="${session}" \
-			--resource="${MSM_ALL_DEDRIFT_RESOURCE_NAME}" \
-			--force
-		
-		db_working_dir=${g_working_dir/HCP/data}/${subject}
-		echo "-------------------------------------------------"
-		echo "Putting new data into DB from db_working_dir: ${db_working_dir}"
-		echo "-------------------------------------------------"
-		${XNAT_PBS_JOBS_HOME}/WorkingDirPut/PutDirIntoResource.sh \
-			--user="${g_user}" \
-			--password="${g_password}" \
-			--server="${g_server}" \
-			--project="${project}" \
-			--subject="${subject}" \
-			--session="${session}" \
-			--resource="${MSM_ALL_DEDRIFT_RESOURCE_NAME}" \
-			--reason="MSMAllRemoveGroupDrift" \
-			--dir="${db_working_dir}" \
-			--force
-
-		echo "-------------------------------------------------"
-		echo "Deleting pushed data"
-		echo "-------------------------------------------------"
-		rm_cmd="rm -rf ${db_working_dir}"
-		echo "rm_cmd: ${rm_cmd}"
-		${rm_cmd}
-		echo "rm_cmd return code: $?"
-	done
-}
-
 push_group_files_to_db()
 {
-	echo "Pushing group stuff - not really, this is just a placeholder"
+	echo "Pushing data to project ${g_project}"
+
+	echo "-------------------------------------------------"
+	echo "Deleting previous resource"	
+	echo "-------------------------------------------------"
+	${XNAT_PBS_JOBS_HOME}/WorkingDirPut/DeleteProjectLevelResource.sh \
+		--user="${g_user}" \
+		--password="${g_password}" \
+		--server="${g_server}" \
+		--project="${g_project}" \
+		--resource="${MSM_ALL_DEDRIFT_RESOURCE_NAME}" \
+		--force
+
+	db_working_dir=${g_working_dir/HCP/data}
+	echo "-------------------------------------------------"
+	echo "Putting new data into DB from db_working_dir: ${db_working_dir}"
+	echo "-------------------------------------------------"
+	${XNAT_PBS_JOBS_HOME}/WorkingDirPut/PutDirIntoProjectLevelResource.sh \
+		--user="${g_user}" \
+		--password="${g_password}" \
+		--server="${g_server}" \
+		--project="${g_project}" \
+		--resource="${MSM_ALL_DEDRIFT_RESOURCE_NAME}" \
+		--reason="MSMAllRemoveGroupDrift" \
+		--dir="${db_working_dir}" \
+		--force
+
+	echo "-------------------------------------------------"
+	echo "Deleting pushed data"
+	echo "-------------------------------------------------"
+	
+	echo "TBD"
 }
 
 #
@@ -233,12 +168,6 @@ main()
 {
 	# get user specified command line options
 	get_options $@
-
-	# get list of subjects
-	get_subject_info_list ${g_subject_info_file}
-
-	# push files generated for individual subjects into DB
-	push_subject_files_to_db
 
 	# push group files into DB
 	push_group_files_to_db
