37babedc0294962c82cc771c4a9feacfc25463ff
Timothy B. Brown <tbbrown@wustl.edu>
2015-10-30 11:51:59 -0500

    First version of GenerateSpinEchoBiasFields

---
+++ GenerateSpinEchoBiasFields/GenerateSpinEchoBiasFields.XNAT.sh
@@ -0,0 +1,484 @@
+#!/bin/bash
+
+#~ND~FORMAT~MARKDOWN~
+#~ND~START~
+#
+# # GenerateSpinEchoBiasFields.XNAT.sh
+#
+# ## Copyright Notice
+#
+# Copyright (C) 2015 The Human Connectome Project
+#
+# * Washington University in St. Louis
+# * University of Minnesota
+# * Oxford University
+#
+# ## Author(s)
+#
+# * Timothy B. Brown, Neuroinformatics Research Group, 
+#   Washington University in St. Louis
+#
+# ## Description
+#
+# TBW
+#
+#~ND~END~
+
+echo "Job started on `hostname` at `date`"
+
+# home directory for scripts to be sourced to setup the environment
+SCRIPTS_HOME=/home/HCPpipeline/SCRIPTS
+echo "SCRIPTS_HOME: ${SCRIPTS_HOME}"
+
+# home directory for XNAT related utilities
+XNAT_UTILS_HOME=/home/HCPpipeline/pipeline_tools/xnat_utilities
+echo "XNAT_UTILS_HOME: ${XNAT_UTILS_HOME}"
+
+# home directory for these XNAT PBS job scripts
+XNAT_PBS_JOBS_HOME=/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs
+echo "XNAT_PBS_JOBS_HOME: ${XNAT_PBS_JOBS_HOME}"
+
+# root directory of the XNAT database archive
+DATABASE_ARCHIVE_ROOT="/HCP/hcpdb/archive"
+echo "DATABASE_ARCHIVE_ROOT: ${DATABASE_ARCHIVE_ROOT}"
+
+# Load Function libraries
+source ${XNAT_PBS_JOBS_HOME}/GetHcpDataUtils/GetHcpDataUtils.sh
+
+# Set up to run Python
+echo "Setting up to run Python"
+source ${SCRIPTS_HOME}/epd-python_setup.sh
+
+# Show script usage information
+usage()
+{
+	echo ""
+	echo "TBW"
+	echo ""
+}
+
+# Parse specified command line options and verify that required options are 
+# specified. "Return" the options to use in global variables
+get_options()
+{
+	local arguments=($@)
+
+	# initialize global output variables
+	unset g_user
+	unset g_password
+	unset g_server
+	unset g_project
+	unset g_subject
+	unset g_session
+	unset g_working_dir
+	unset g_workflow_id
+
+	# parse arguments
+	local num_args=${#arguments[@]}
+	local argument
+	local index=0
+
+	while [ ${index} -lt ${num_args} ]; do
+		argument=${arguments[index]}
+
+		case ${argument} in
+			--help)
+				usage
+				exit 1
+				;;
+			--user=*)
+				g_user=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--password=*)
+				g_password=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--server=*)
+				g_server=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--project=*)
+				g_project=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--subject=*)
+				g_subject=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--session=*)
+				g_session=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--working-dir=*)
+				g_working_dir=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--workflow-id=*)
+				g_workflow_id=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			*)
+				usage
+				echo "ERROR: unrecognized option: ${argument}"
+				echo ""
+				exit 1
+				;;
+		esac
+	done
+
+	local error_count=0
+
+	# check required parameters
+	if [ -z "${g_user}" ]; then
+		echo "ERROR: user (--user=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_user: ${g_user}"
+	fi
+
+	if [ -z "${g_password}" ]; then
+		echo "ERROR: password (--password=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_password: *******"
+	fi
+
+	if [ -z "${g_server}" ]; then
+		echo "ERROR: server (--server=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_server: ${g_server}"
+	fi
+
+	if [ -z "${g_project}" ]; then
+		echo "ERROR: project (--project=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_project: ${g_project}"
+	fi
+
+	if [ -z "${g_subject}" ]; then
+		echo "ERROR: subject (--subject=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_subject: ${g_subject}"
+	fi
+
+	if [ -z "${g_session}" ]; then
+		echo "ERROR: session (--session=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_session: ${g_session}"
+	fi
+
+	if [ -z "${g_working_dir}" ]; then
+		echo "ERROR: working directory (--working-dir=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_working_dir: ${g_working_dir}"
+	fi
+
+	if [ ! -z "${g_workflow_id}" ]; then
+		echo "g_workflow_id: ${g_workflow_id}"
+	fi
+
+	if [ ${error_count} -gt 0 ]; then
+		echo "For usage information, use --help"
+		exit 1
+	fi
+}
+
+# Show information about a specified XNAT Workflow
+show_xnat_workflow()
+{
+	if [ ! -z "${g_workflow_id}" ]; then
+		${XNAT_UTILS_HOME}/xnat_workflow_info \
+			--server="${g_server}" \
+			--username="${g_user}" \
+			--password="${g_password}" \
+			--workflow-id="${g_workflow_id}" \
+			show
+	fi
+}
+
+# Update information (step id, step description, and percent complete)
+# for a specified XNAT Workflow
+update_xnat_workflow()
+{
+	local step_id=${1}
+	local step_desc=${2}
+	local percent_complete=${3}
+
+	echo ""
+	echo ""
+	echo "---------- Step: ${step_id} "
+	echo "---------- Desc: ${step_desc} "
+	echo ""
+	echo ""
+
+	if [ ! -z "${g_workflow_id}" ]; then
+		echo "update_xnat_workflow - workflow_id: ${g_workflow_id}"
+		echo "update_xnat_workflow - step_id: ${step_id}"
+		echo "update_xnat_workflow - set_desc: ${step_desc}"
+		echo "update_xnat_workflow - percent_complete: ${percent_complete}"
+
+		${XNAT_UTILS_HOME}/xnat_workflow_info \
+			--server="${g_server}" \
+			--username="${g_user}" \
+			--password="${g_password}" \
+			--workflow-id="${g_workflow_id}" \
+			update \
+			--step-id="${step_id}" \
+			--step-description="${step_desc}" \
+			--percent-complete="${percent_complete}"
+	fi
+}
+
+# Mark the specified XNAT Workflow as complete
+complete_xnat_workflow()
+{
+	if [ ! -z "${g_workflow_id}" ]; then
+		${XNAT_UTILS_HOME}/xnat_workflow_info \
+			--server="${g_server}" \
+			--username="${g_user}" \
+			--password="${g_password}" \
+			--workflow-id="${g_workflow_id}" \
+			complete
+	fi
+}
+
+# Mark the specified XNAT Workflow as failed
+fail_xnat_workflow()
+{
+	if [ ! -z "${g_workflow_id}" ]; then
+		${XNAT_UTILS_HOME}/xnat_workflow_info \
+			--server="${g_server}" \
+			--username="${g_user}" \
+			--password="${g_password}" \
+			--workflow-id="${g_workflow_id}" \
+			fail
+	fi
+}
+
+# Update specified XNAT Workflow to Failed status and exit this script
+die()
+{
+	fail_xnat_workflow ${g_workflow_id}
+	exit 1
+}
+
+# Initialize the step counters
+init_steps()
+{
+	local total_steps=${1}
+	g_total_steps=${total_steps}
+	g_current_step=0
+}
+
+# Increment the current step
+increment_step()
+{
+	g_current_step=$(( g_current_step + 1 ))
+	if [ ${g_current_step} -gt ${g_total_steps} ] ; then
+		echo "ERROR: g_current_step: ${g_current_step} greater than g_total_steps: ${g_total_steps}"
+		exit 1
+	fi
+	g_step_percent=$(( (g_current_step * 100) / g_total_steps ))
+}
+
+# Main processing
+#   Carry out the necessary steps to:
+#   - get prerequisite data 
+#   - run the script
+main()
+{
+	get_options $@
+
+	# Set up step counters
+	init_steps 9
+
+	show_xnat_workflow
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Figure out what resting state scans are available for this subject/session
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Figure out what resting state scans should be processed" ${g_step_percent}
+
+	pushd ${DATABASE_ARCHIVE_ROOT}/${g_project}/arc001/${g_session}/RESOURCES
+
+	resting_state_scan_names=""
+	resting_state_scan_dirs=`ls -d rfMRI_REST*_preproc`
+	for resting_state_scan_dir in ${resting_state_scan_dirs} ; do
+		scan_name=${resting_state_scan_dir%%_preproc}
+		resting_state_scan_names+="${scan_name} "
+	done
+	resting_state_scan_names=${resting_state_scan_names% } # remove trailing space
+	
+	if [ -z "${resting_state_scan_names}" ]; then
+		resting_state_scan_names="NONE"
+	fi
+
+	echo "Found the following resting state scans: ${resting_state_scan_names}"
+
+	popd
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Figure out what task scans are available for this subject/session 
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Figure out what task scans should be processed" ${g_step_percent}
+
+	pushd ${DATABASE_ARCHIVE_ROOT}/${g_project}/arc001/${g_session}/RESOURCES
+
+	task_scan_names=""
+	task_scan_dirs=`ls -d tfMRI_*_preproc`
+	for task_scan_dir in ${task_scan_dirs} ; do
+		scan_name=${task_scan_dir%%_preproc}
+		task_scan_names+="${scan_name} "
+	done
+	task_scan_names=${task_scan_names% } # remove trailing space
+
+	if [ -z "${task_scan_names}" ]; then
+		task_scan_names="NONE"
+	fi
+
+	echo "Found the following task scans: ${task_scan_names}"
+
+	popd
+
+	# VERY IMPORTANT NOTE:
+	#
+	# Since ConnectomeDB resources contain overlapping files (e.g. the functionally preprocessed
+	# data resource may contain some of the exact same files as the structurally preprocessed
+	# data resource), extra care must be taken with the order in which data is linked in to the
+	# working directory.
+	#
+	# If, for example, a file named ${subject}/subdir1/subdir2/this_file.nii.gz exists in both
+	# the structurally preprocessed data resource and in the functionally preprocessed data 
+	# resource, whichever resource we link in to the working directory _first_ will take 
+	# precedence.  (This is due to the behavior of the lndir command used by the link_hcp...
+	# functions, and is unlike the behavior of the rsync command used by the get_hcp...
+	# functions. The rsync command will copy/update the newer version of the file from its 
+	# source.)
+	#
+	# The lndir command will report to stderr any links that it could not (would not) create 
+	# because they already exist in the destination directories.
+	#
+	# So, if we link in the structurally preprocessed data first and then link in the functionally
+	# preprocessed data second, the file ${subject}/subdir1/subdir2/this_file.nii.gz in the 
+	# working directory will be linked back to the structurally preprocessed version of the file.
+	#
+	# Since functional preprocessing comes _after_ structural preprocessing, this is not likely
+	# to be what we want.  Instead, we want the file as it exists after functional preprocessing
+	# to be the one that is linked in to the working directory.
+	#
+	# Therefore, it is important to consider the order in which we call the link_hcp... functions
+	# below.  We should call them in order from the results of the latest prerequisite pipelines
+	# to the earliest prerequisite pipelines.
+
+	# ----------------------------------------------------------------------------------------------
+ 	# Step - Link functionally preprocessed data from DB
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Link functionally preprocessed data from DB" ${g_step_percent}
+
+	for scan_name in ${resting_state_scan_names} ${task_scan_names} ; do
+		link_hcp_func_preproc_data "${DATABASE_ARCHIVE_ROOT}" "${g_project}" "${g_subject}" "${g_session}" "${scan_name}" "${g_working_dir}"
+	done
+	
+	# ----------------------------------------------------------------------------------------------
+ 	# Step - Link structurally preprocessed data from DB
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Link structurally preprocessed data from DB" ${g_step_percent}
+
+	link_hcp_struct_preproc_data "${DATABASE_ARCHIVE_ROOT}" "${g_project}" "${g_subject}" "${g_session}" "${g_working_dir}"
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Create a start_time file
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Create a start_time file" ${g_step_percent}
+	
+	start_time_file="${g_working_dir}/GenerateSpinEchoBiasFields.starttime"
+	if [ -e "${start_time_file}" ]; then
+		echo "Removing old ${start_time_file}"
+		rm -f ${start_time_file}
+	fi
+	
+	# Sleep for 1 minute to make sure start_time file is created at least a
+	# minute after any files copied or linked above.
+	echo "Sleep for 1 minute before creating start_time file."
+	sleep 1m || die
+
+	echo "Creating start time file: ${start_time_file}"
+	touch ${start_time_file} || die 
+	ls -l ${start_time_file}
+
+	# Sleep for 1 minute to make sure any files created or modified by the script below
+	# are created at least 1 minute after the start_time file
+	echo "Sleep for 1 minute after creating start_time file."
+	sleep 1m || die 
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Run GenerateSpinEchoBiasFields.sh script
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Run GenerateSpinEchoBiasFields.sh script" ${g_step_percent}
+	
+	# Source setup script to setup environment for running the script
+	setup_file="${SCRIPTS_HOME}/SetUpHCPPipeline_GenerateSpinEchoBiasFields.sh"
+	if [ ! -e ${setup_file} ] ; then
+		echo "ERROR: setup_file: ${setup_file} DOES NOT EXIST! ABORTING"
+		die
+	fi
+
+	source ${setup_file}
+
+	# Setup variables for command line arguments
+	local rfMRINames="${resting_state_scan_names}" #List of Resting State Maps Space delimited list or NONE
+	local tfMRINames="${task_scan_names}" #Space delimited list or NONE
+
+	rfMRINames=`echo "$rfMRINames" | sed s/" "/"@"/g`
+	tfMRINames=`echo "$tfMRINames" | sed s/" "/"@"/g`
+
+	# Run DeDriftAndResamplePipeline.sh script
+	${HCPPIPEDIR}/GenerateSpinEchoBiasFields/GenerateSpinEchoBiasFields.sh \
+		--path=${g_working_dir} \
+		--subject=${g_subject} \
+		--rfmri-names="${rfMRINames}" \
+		--tfmri-names="${tfMRINames}"
+	
+	if [ $? -ne 0 ]; then
+		die 
+	fi
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Show any newly created or modified files
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Show newly created or modified files" ${g_step_percent}
+	
+	echo "Newly created/modified files:"
+	find ${g_working_dir}/${g_subject} -type f -newer ${start_time_file}
+	
+	# ----------------------------------------------------------------------------------------------
+	# Step - Remove any files that are not newly created or modified
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	update_xnat_workflow ${g_current_step} "Remove files not newly created or modified" ${g_step_percent}
+
+	echo "The following files are being removed"
+	find ${g_working_dir} -not -newer ${start_time_file} -print -delete || die 
+	
+	# ----------------------------------------------------------------------------------------------
+	# Step - Complete Workflow
+	# ----------------------------------------------------------------------------------------------
+	increment_step
+	complete_xnat_workflow 
+}
+
+# Invoke the main function to get things started
+main $@
---
+++ GenerateSpinEchoBiasFields/SubmitGenerateSpinEchoBiasFields.OneSubject.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+
+# home directory for these XNAT PBS job scripts
+XNAT_PBS_JOBS_HOME=/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs
+echo "XNAT_PBS_JOBS_HOME: ${XNAT_PBS_JOBS_HOME}"
+
+# home directory for XNAT pipeline engine installation
+XNAT_PIPELINE_HOME=/home/HCPpipeline/pipeline
+echo "XNAT_PIPELINE_HOME: ${XNAT_PIPELINE_HOME}"
+
+# home directory for XNAT utilities
+XNAT_UTILS_HOME=/home/HCPpipeline/pipeline_tools/xnat_utilities
+echo "XNAT_UTILS_HOME: ${XNAT_UTILS_HOME}"
+
+# main build directory
+BUILD_HOME="/HCP/hcpdb/build_ssd/chpc/BUILD"
+echo "BUILD_HOME: ${BUILD_HOME}"
+
+# set up to run Python
+echo "Setting up to run Python"
+source ${SCRIPTS_HOME}/epd-python_setup.sh
+
+# Database Resource names and suffixes
+echo "Defining Database Resource Names and Suffixes"
+source ${XNAT_PBS_JOBS_HOME}/GetHcpDataUtils/ResourceNamesAndSuffixes.sh
+
+get_options() 
+{
+	local arguments=($@)
+
+	# initialize global output variables
+	unset g_user
+	unset g_password
+	unset g_server
+	unset g_project
+	unset g_subject
+	unset g_session
+	unset g_notify
+
+	# parse arguments
+	local num_args=${#arguments[@]}
+	local argument
+	local index=0
+
+	while [ ${index} -lt ${num_args} ]; do
+		argument=${arguments[index]}
+
+		case ${argument} in
+			--user=*)
+				g_user=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--password=*)
+				g_password=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--server=*)
+				g_server=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--project=*)
+				g_project=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--subject=*)
+				g_subject=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--session=*)
+				g_session=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--notify=*)
+				g_notify=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			*)
+				echo "ERROR: unrecognized option: ${argument}"
+				echo ""
+				exit 1
+				;;
+		esac
+	done
+
+	# set defaults and prompt for some unspecified parameters
+	if [ -z "${g_user}" ]; then
+		printf "Enter Connectome DB Username: "
+		read g_user
+	fi
+
+	if [ -z "${g_password}" ]; then
+		stty -echo
+		printf "Enter Connectome DB Password: "
+		read g_password
+		echo ""
+		stty echo
+	fi
+
+	if [ -z "${g_server}" ]; then
+		g_server="db.humanconnectome.org"
+	fi
+	echo "Connectome DB Server: ${g_server}"
+
+	if [ -z "${g_project}" ]; then
+		g_project="HCP_500"
+	fi
+    echo "Connectome DB Project: ${g_project}"
+
+	if [ -z "${g_subject}" ]; then
+		printf "Enter Connectome DB Subject: "
+		read g_subject
+	fi
+	echo "Connectome DB Subject: ${g_subject}"
+
+	if [ -z "${g_session}" ]; then
+		g_session=${g_subject}_3T
+	fi
+	echo "Connectome DB Session: ${g_session}"
+
+	echo "Notification Email: ${g_notify}"
+}
+
+main()
+{
+	get_options $@
+
+	# Get token user id and password
+	echo "Getting token user id and password"
+	get_token_cmd="${XNAT_UTILS_HOME}/xnat_get_tokens --server=${g_server} --username=${g_user}"
+	echo "get_token_cmd: ${get_token_cmd}"
+	get_token_cmd+=" --password=${g_password}"
+	new_tokens=`${get_token_cmd}`
+	token_username=${new_tokens% *}
+	token_password=${new_tokens#* }
+	echo "token_username: ${token_username}"
+	echo "token_password: ${token_password}"
+
+	# make sure working directories don't have the same name based on the 
+	# same start time by sleeping a few seconds
+	sleep 5s
+
+	current_seconds_since_epoch=`date +%s`
+	working_directory_name="${BUILD_HOME}/${g_project}/GenerateSpinEchoBiasFields_${current_seconds_since_epoch}_${g_subject}"
+
+	# Make the working directory
+	echo "Making working directory: ${working_directory_name}"
+	mkdir -p ${working_directory_name}
+
+	# Get JSESSION ID
+	echo "Getting JSESSION ID"
+	jsession=`curl -u ${g_user}:${g_password} https://db.humanconnectome.org/data/JSESSION`
+	echo "jsession: ${jsession}"
+
+	# Get XNAT Session ID (a.k.a. the experiment ID, e.g. ConnectomeDB_E1234)
+	echo "Getting XNAT Session ID"
+	get_session_id_cmd=""
+	get_session_id_cmd+="python ${XNAT_PIPELINE_HOME}/catalog/ToolsHCP/resources/scripts/sessionid.py "
+	get_session_id_cmd+="--server=db.humanconnectome.org "
+	get_session_id_cmd+="--username=${g_user} "
+	get_session_id_cmd+="--project=${g_project} "
+	get_session_id_cmd+="--subject=${g_subject} "
+	get_session_id_cmd+="--session=${g_session} "
+	echo "get_session_id_cmd: ${get_session_id_cmd}"
+	get_session_id_cmd+=" --password=${g_password}"
+
+	sessionID=`${get_session_id_cmd}`
+	echo "XNAT session ID: ${sessionID}"
+
+	# Get XNAT Workflow ID
+	server="https://db.humanconnectome.org/"
+	echo "Getting XNAT workflow ID for this job from server: ${server}"
+	get_workflow_id_cmd=""
+	get_workflow_id_cmd+="python ${XNAT_PIPELINE_HOME}/catalog/ToolsHCP/resources/scripts/workflow.py "
+	get_workflow_id_cmd+="-User ${g_user} "
+	get_workflow_id_cmd+="-Server ${server} "
+	get_workflow_id_cmd+="-ExperimentID ${sessionID} "
+	get_workflow_id_cmd+="-ProjectID ${g_project} "
+	get_workflow_id_cmd+="-Pipeline DeDriftAndResample "
+	get_workflow_id_cmd+="-Status Queued "
+	get_workflow_id_cmd+="-JSESSION ${jsession} "
+	echo "get_workflow_id_cmd: ${get_workflow_id_cmd}"
+	get_workflow_id_cmd+=" -Password ${g_password}"
+
+	workflowID=`${get_workflow_id_cmd}`
+	if [ $? -ne 0 ]; then
+		echo "Fetching workflow failed. Aborting"
+		echo "workflowID: ${workflowID}"
+		exit 1
+	elif [[ ${workflowID} == HTTP* ]]; then
+		echo "Fetching workflow failed. Aborting"
+		echo "worflowID: ${workflowID}"
+		exit 1
+	fi
+	echo "XNAT workflow ID: ${workflowID}"
+
+	# Submit job to actually do the work
+	script_file_to_submit=${working_directory_name}/${g_subject}.GenerateSpinEchoBiasFields.${g_project}.${g_session}.${current_seconds_since_epoch}.XNAT_PBS_job.sh
+	if [ -e "${script_file_to_submit}" ]; then
+		rm -f "${script_file_to_submit}"
+	fi
+
+	touch ${script_file_to_submit}
+	echo "#PBS -l nodes=1:ppn=1,walltime=8:00:00,vmem=16000mb" >> ${script_file_to_submit}
+	echo "#PBS -q dque" >> ${script_file_to_submit}
+	echo "#PBS -o ${working_directory_name}" >> ${script_file_to_submit}
+	echo "#PBS -e ${working_directory_name}" >> ${script_file_to_submit}
+	if [ -n "${g_notify}" ]; then
+		echo "#PBS -M ${g_notify}" >> ${script_file_to_submit}
+		echo "#PBS -m abe" >> ${script_file_to_submit}
+	fi
+	echo ""
+	echo "/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/GenerateSpinEchoBiasFields/GenerateSpinEchoBiasFields.XNAT.sh \\" >> ${script_file_to_submit}
+	echo "  --user=\"${token_username}\" \\" >> ${script_file_to_submit}
+	echo "  --password=\"${token_password}\" \\" >> ${script_file_to_submit}
+	echo "  --server=\"${g_server}\" \\" >> ${script_file_to_submit}
+	echo "  --project=\"${g_project}\" \\" >> ${script_file_to_submit}
+	echo "  --subject=\"${g_subject}\" \\" >> ${script_file_to_submit}
+	echo "  --session=\"${g_session}\" \\" >> ${script_file_to_submit}
+	echo "  --working-dir=\"${working_directory_name}\" \\" >> ${script_file_to_submit}
+	echo "  --workflow-id=\"${workflowID}\" " >> ${script_file_to_submit}
+
+	submit_cmd="qsub ${script_file_to_submit}"
+	echo "submit_cmd: ${submit_cmd}"
+	
+	processing_job_no=`${submit_cmd}`
+	echo "processing_job_no: ${processing_job_no}"
+	
+	# Submit job to put the results in the DB
+	put_script_file_to_submit=${working_directory_name}/${g_subject}.GenerateSpinEchoBiasFields.${g_project}.${g_session}.${current_seconds_since_epoch}.XNAT_PBS_PUT_job.sh
+	if [ -e "${put_script_file_to_submit}" ]; then
+		rm -f "${put_script_file_to_submit}"
+	fi
+		
+	touch ${put_script_file_to_submit}
+	echo "#PBS -l nodes=1:ppn=1,walltime=4:00:00,vmem=4000mb" >> ${put_script_file_to_submit}
+	echo "#PBS -q HCPput" >> ${put_script_file_to_submit}
+	echo "#PBS -o ${LOG_DIR}" >> ${put_script_file_to_submit}
+	echo "#PBS -e ${LOG_DIR}" >> ${put_script_file_to_submit}
+	
+	if [ -n "${g_notify}" ]; then
+		echo "#PBS -M ${g_notify}" >> ${put_script_file_to_submit}
+		echo "#PBS -m abe" >> ${put_script_file_to_submit}
+	fi
+	echo ""
+	echo "/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/WorkingDirPut/XNAT_working_dir_put.sh \\" >> ${put_script_file_to_submit}
+	echo "  --user=\"${token_username}\" \\" >> ${put_script_file_to_submit}
+	echo "  --password=\"${token_password}\" \\" >> ${put_script_file_to_submit}
+	echo "  --server=\"${g_server}\" \\" >> ${put_script_file_to_submit}
+	echo "  --project=\"${g_project}\" \\" >> ${put_script_file_to_submit}
+	echo "  --subject=\"${g_subject}\" \\" >> ${put_script_file_to_submit}
+	echo "  --session=\"${g_session}\" \\" >> ${put_script_file_to_submit}
+	echo "  --working-dir=\"${working_directory_name}\" \\" >> ${put_script_file_to_submit}
+	echo "  --resource-suffix=\"${SPIN_ECHO_BIAS_FIELDS_RESOURCE_NAME}\" \\" >> ${put_script_file_to_submit} 
+	echo "  --reason=\"GenerateSpinEchoBiasFields\" " >> ${put_script_file_to_submit}
+	
+	submit_cmd="qsub -W depend=afterok:${processing_job_no} ${put_script_file_to_submit}"
+	echo "submit_cmd: ${submit_cmd}"
+	${submit_cmd}
+}
+
+# Invoke the main function to get things started
+main $@
+
+
