ac1330277799bd70de1d947a34c30d03a2850b93
Timothy B. Brown <tbbrown@wustl.edu>
2015-12-15 12:02:18 -0600

    Added convenience script for submitting RestingStateStats jobs for sessions in the HCP_Staging_RT project

---
+++ RestingStateStats/SubmitRestingStateStats.HCP_Staging_RT.Batch.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [ -z "${SUBJECT_FILES_DIR}" ]; then
+	echo "Environment variable SUBJECT_FILES_DIR must be set!"
+	exit 1
+fi
+
+printf "Connectome DB Username: "
+read userid
+
+stty -echo
+printf "Connectome DB Password: "
+read password
+echo ""
+stty echo
+
+printf "Delay until first submission (minutes) [0]: "
+read delay
+
+if [ -z "${delay}" ]; then
+	delay=0
+fi
+
+printf "Interval between submissions (minutes) [60]: "
+read interval
+
+if [ -z "${interval}" ]; then
+	interval=60
+fi
+
+project="HCP_Staging_RT"
+subject_file_name="${SUBJECT_FILES_DIR}/${project}.RestingStateStats.subjects"
+echo "Retrieving subject list from: ${subject_file_name}"
+subject_list_from_file=( $( cat ${subject_file_name} ) )
+subjects="`echo "${subject_list_from_file[@]}"`"
+
+start_shadow_number=1
+max_shadow_number=8
+
+shadow_number=${start_shadow_number}
+
+for subject in ${subjects} ; do
+
+	if [[ ${subject} != \#* ]]; then
+
+		server="db-shadow${shadow_number}.nrg.mir:8080"
+
+ 		echo ""
+		echo "--------------------------------------------------------------------------------"
+		echo " Submitting RestingStateStats jobs for subject: ${subject}"
+		echo " Using server: ${server}"
+		echo " Submission delayed until ${delay} minutes from now"
+		echo "--------------------------------------------------------------------------------"
+
+		at now + ${delay} minutes <<EOF 
+			/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/RestingStateStats/SubmitRestingStateStats.OneSubject.sh \
+			--user=${userid} \
+			--password=${password} \
+			--server=${server} \
+			--project=${project} \
+			--subject=${subject} 
+EOF
+
+		delay=$((delay + interval))
+
+		shadow_number=$((shadow_number+1))
+		
+		if [ "${shadow_number}" -gt "${max_shadow_number}" ]; then
+			shadow_number=${start_shadow_number}
+		fi
+
+	fi
+
+done
