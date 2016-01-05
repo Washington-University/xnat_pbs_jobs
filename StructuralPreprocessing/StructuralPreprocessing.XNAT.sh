adda145a0efa262b8daf124fa327c63f801a979e
Timothy B. Brown <tbbrown@wustl.edu>
2016-01-05 16:30:43 -0600

    First, non-working version of StructuralPreprocessing.XNAT.sh

---
+++ StructuralPreprocessing/StructuralPreprocessing.XNAT.sh
@@ -0,0 +1,242 @@
+#!/bin/bash
+
+#~ND~FORMAT~MARKDOWN~
+#~ND~START~
+#
+# # StructuralPreprocessing.XNAT.sh
+#
+# ## Copyright Notice
+#
+# Copyright (C) 2016 The Human Connectome Project
+#
+# * Washington University in St. Louis
+# * University of Minnesota
+# * Oxford University
+#
+# ## Author(s)
+#
+# * Timothy B. Brown, Neuroinformatics Research Group, 
+#   Washington University in St. Louis
+#
+# ## Description
+#
+# This script runs the Structural Preprocessing pipeline consisting of the 
+# PreFreeSurfer, FreeSurfer, and PostFreeSurfer pipeline scripts from the Human 
+# Connectome Project for a specified project, subject, session, and scan 
+# in the ConnectomeDB (db.humanconnectome.org) XNAT database.
+#
+# The script is run not as an XNAT pipeline (under the control of the
+# XNAT Pipeline Engine), but in an "XNAT-aware" and "pipeline-like" manner.
+# 
+# The data to be processed is retrieved via filesystem operations instead
+# of using REST API calls to retrieve that data. So the database archive
+# and resource directory structure is "known and used" by this script.
+# 
+# This script can be invoked by a job submitted to a worker or execution
+# node in a cluster, e.g. a Sun Grid Engine (SGE) managed or Portable Batch
+# System (PBS) managed cluster. Alternatively, if the machine being used
+# has adequate resources (RAM, CPU power, storage space), this script can 
+# simply be invoked interactively.
+#
+#~ND~END~
+
+echo "Job started on `hostname` at `date`"
+
+# home directory for scripts to be sourced to setup the environment
+SCRIPTS_HOME=${HOME}/SCRIPTS
+echo "SCRIPTS_HOME: ${SCRIPTS_HOME}"
+
+# home directory for pipeline tools
+PIPELINE_TOOLS_HOME=${HOME}/pipeline_tools
+echo "PIPELINE_TOOLS_HOME: ${PIPELINE_TOOLS_HOME}"
+
+# home directory for XNAT related utilities
+XNAT_UTILS_HOME=${PIPELINE_TOOLS_HOME}/xnat_utilities
+echo "XNAT_UTILS_HOME: ${XNAT_UTILS_HOME}"
+
+# home directory for these XNAT PBS job scripts
+XNAT_PBS_JOBS_HOME=${PIPELINE_TOOLS_HOME}/xnat_pbs_jobs
+echo "XNAT_PBS_JOBS_HOME: ${XNAT_PBS_JOBS_HOME}"
+
+# root directory of the XNAT database archive
+DATABASE_ARCHIVE_ROOT="/HCP/hcpdb/archive"
+echo "DATABASE_ARCHIVE_ROOT: ${DATABASE_ARCHIVE_ROOT}"
+
+# Show script usage information
+usage()
+{
+	echo ""
+	echo "  Run the HCP Structural Preprocessing pipeline scripts (PreFreeSurferPipeline.sh, "
+	echo "  FreeSurferPipeline.sh, and PostFreeSurferPipeline.sh) in an XNAT-aware and "
+	echo "  XNAT-pipeline-like manner."
+	echo ""
+	echo "  Usage: StructuralPreprocessing.XNAT.sh <options>"
+	echo ""
+	echo "  Options: [ ] = optional, < > = user-supplied-value"
+	echo ""
+	echo "   [--help] : show usage information and exit"
+	echo ""
+	echo "    --user=<username>      : XNAT DB username"
+	echo "    --password=<password>  : XNAT DB password"
+	echo "    --server=<server>      : XNAT server (e.g. db.humanconnectome.org)"
+	echo "    --project=<project>    : XNAT project (e.g. HCP_500)"
+	echo "    --subject=<subject>    : XNAT subject ID within project (e.g. 100307)"
+	echo "    --session=<session>    : XNAT session ID within project (e.g. 100307_3T)"
+	echo "    --working-dir=<dir>    : Working directory in which to place retrieved data"
+	echo "                             and in which to produce results"
+	echo "    --workflow-id=<id>     : XNAT Workflow ID to update as steps are completed"
+	echo ""
+}
+
+# Parse specified command line options and verify that required options are 
+# specified. "Return" the options to use in global variables
+get_options()
+{
+	local arguments=($@)
+
+	# initialize global output variables
+	unset g_user
+	unset g_password
+	unset g_server
+	unset g_project
+	unset g_subject
+	unset g_session
+	unset g_working_dir
+	unset g_workflow_id
+	
+	# parse arguments
+	local num_args=${#arguments[@]}
+	local argument
+	local index=0
+
+	while [ ${index} -lt ${num_args} ]; do
+		argument=${arguments[index]}
+
+		case ${argument} in
+			--help)
+				usage
+				exit 1
+				;;
+			--user=*)
+				g_user=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--password=*)
+				g_password=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--server=*)
+				g_server=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--project=*)
+				g_project=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--subject=*)
+				g_subject=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--session=*)
+				g_session=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--working-dir=*)
+				g_working_dir=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--workflow-id=*)
+				g_workflow_id=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			*)
+				usage
+				echo "ERROR: unrecognized option: ${argument}"
+				echo ""
+				exit 1
+				;;
+		esac
+	done
+
+	local error_count=0
+
+	# check required parameters
+	if [ -z "${g_user}" ]; then
+		echo "ERROR: user (--user=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_user: ${g_user}"
+	fi
+
+	if [ -z "${g_password}" ]; then
+		echo "ERROR: password (--password=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_password: *******"
+	fi
+
+	if [ -z "${g_server}" ]; then
+		echo "ERROR: server (--server=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_server: ${g_server}"
+	fi
+
+	if [ -z "${g_project}" ]; then
+		echo "ERROR: project (--project=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_project: ${g_project}"
+	fi
+
+	if [ -z "${g_subject}" ]; then
+		echo "ERROR: subject (--subject=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_subject: ${g_subject}"
+	fi
+
+	if [ -z "${g_session}" ]; then
+		echo "ERROR: session (--session=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_session: ${g_session}"
+	fi
+
+	if [ -z "${g_working_dir}" ]; then
+		echo "ERROR: working directory (--working-dir=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_working_dir: ${g_working_dir}"
+	fi
+
+	if [ -z "${g_workflow_id}" ]; then
+		echo "ERROR: workflow ID (--workflow-id=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_workflow_id: ${g_workflow_id}"
+	fi
+
+	if [ ${error_count} -gt 0 ]; then
+		echo "For usage information, use --help"
+		exit 1
+	fi
+}
+
+# Main processing
+#   Carry out the necessary steps to:
+#   - get prerequisite data for the Strucutral Preprocessing pipeline 
+#   - run the scripts
+main()
+{
+	get_options $@
+
+	source ${XNAT_UTILS_HOME}/xnat_workflow_utilities.sh
+	source ${XNAT_PBS_JOBS_HOME}/GetHcpDataUtils/GetHcpDataUtils.sh
+
+
+
+}
+
+# Invoke the main function to get things started
+main $@
