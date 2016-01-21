a6503a477f1594aa4bffbe73cda36abc0640aa0f
Timothy B. Brown <tbbrown@wustl.edu>
2016-01-21 11:49:14 -0600

    Diffusion Preprocessing in work

---
+++ DiffusionPreprocessingHCP/DiffusionPreprocessingHCP_PreEddy.XNAT.sh
@@ -0,0 +1,440 @@
+#!/bin/bash
+
+#~ND~FORMAT~MARKDOWN~
+#~ND~START~
+#
+# # DiffusionPreprocessingHCP_PreEddy.XNAT.sh
+#
+# ## Copyright Notice
+#
+# Copyright (C) 2016 The Human Connectome Project
+#
+# * Washington University in St. Louis
+# * University of Minnesota
+# * Oxford University
+#
+# ## Author(s)
+#
+# * Timothy B. Brown, Neuroinformatics Research Group, 
+#   Washington University in St. Louis
+#
+# ## Description
+#
+# This script runs the Diffusion Preprocessing pipeline PreEddy phase 
+# consisting of the DiffPreprocPipeline_PreEddy.sh pipeline script from
+# the Human Connectome Project for a specified project, subject, session,
+
+#  and scan 
+
+# in the ConnectomeDB (db.humanconnectome.org) XNAT database.
+#
+# The script is run not as an XNAT pipeline (under the control of the
+# XNAT Pipeline Engine), but in an "XNAT-aware" and "pipeline-like" manner.
+# 
+# The data to be processed are retrieved via filesystem operations instead
+# of using REST API calls. So the database archive and resource directory 
+# structure is "known and used" by this script.
+# 
+# This script can be invoked by a job submitted to a worker or execution
+# node in a cluster, e.g. a Sun Grid Engine (SGE) managed or Portable Batch
+# System (PBS) managed cluster. Alternatively, if the machine being used
+# has adequate resources (RAM, CPU power, storage space), this script can 
+# simply be invoked interactively.
+#
+#~ND~END~
+
+echo "Job started on `hostname` at `date`"
+
+# home directory for scripts to be sourced to setup the environment
+SCRIPTS_HOME=${HOME}/SCRIPTS
+echo "SCRIPTS_HOME: ${SCRIPTS_HOME}"
+
+# home directory for pipeline tools
+PIPELINE_TOOLS_HOME=${HOME}/pipeline_tools
+echo "PIPELINE_TOOLS_HOME: ${PIPELINE_TOOLS_HOME}"
+
+# home directory for XNAT related utilities
+XNAT_UTILS_HOME=${PIPELINE_TOOLS_HOME}/xnat_utilities
+echo "XNAT_UTILS_HOME: ${XNAT_UTILS_HOME}"
+
+# home directory for these XNAT PBS job scripts
+XNAT_PBS_JOBS_HOME=${PIPELINE_TOOLS_HOME}/xnat_pbs_jobs
+echo "XNAT_PBS_JOBS_HOME: ${XNAT_PBS_JOBS_HOME}"
+
+# home directory for XNAT pipeline engine installation
+XNAT_PIPELINE_HOME=/home/HCPpipeline/pipeline
+
+# root directory of the XNAT database archive
+DATABASE_ARCHIVE_ROOT="/HCP/hcpdb/archive"
+echo "DATABASE_ARCHIVE_ROOT: ${DATABASE_ARCHIVE_ROOT}"
+
+# source XNAT workflow utility functions
+source ${XNAT_UTILS_HOME}/xnat_workflow_utilities.sh
+
+# source utility functions for getting data
+source ${XNAT_PBS_JOBS_HOME}/GetHcpDataUtils/GetHcpDataUtils.sh
+
+# set up to run Python
+source ${SCRIPTS_HOME}/epd-python_setup.sh
+
+RLLR_POSITIVE_DIR="RL"
+RLLR_NEGATIVE_DIR="LR"
+
+PAAP_POSITIVE_DIR="PA"
+PAAP_NEGATIVE_DIR="AP"
+
+# Show script usage information
+usage()
+{
+	echo ""
+	echo "  Run the HCP Diffusion Preprocessing pipeline PreEddy phase script "
+	echo "  (DiffPreprocPipeline_PreEddy.sh) in an XNAT-aware and "
+	echo "  XNAT-pipeline-like manner."
+	echo ""
+	echo "  Usage: DiffusionPreprocessingHCP_PreEddy.XNAT.sh <options>"
+	echo ""
+	echo "  Options: [ ] = optional, < > = user-supplied-value"
+	echo ""
+	echo "   [--help] : show usage information and exit"
+	echo ""
+	echo "    --user=<username>      : XNAT DB username"
+	echo "    --password=<password>  : XNAT DB password"
+	echo "    --server=<server>      : XNAT server (e.g. db.humanconnectome.org)"
+	echo "    --project=<project>    : XNAT project (e.g. HCP_500)"
+	echo "    --subject=<subject>    : XNAT subject ID within project (e.g. 100307)"
+	echo "    --session=<session>    : XNAT session ID within project (e.g. 100307_3T)"
+	echo "    --working-dir=<dir>    : Working directory in which to place retrieved data"
+	echo "                             and in which to produce results"
+	echo "    --workflow-id=<id>     : XNAT Workflow ID to update as steps are completed"
+	echo "    --phase-encoding-dir=<dir-indication>"
+	echo "                           : One of {RLLR, PAAP}"
+	echo ""
+}
+
+
+# Parse specified command line options and verify that required options are 
+# specified. "Return" the options to use in global variables
+get_options()
+{
+	local arguments=($@)
+
+	# initialize global output variables
+	unset g_user
+	unset g_password
+	unset g_server
+	unset g_project
+	unset g_subject
+	unset g_session
+	unset g_working_dir
+	unset g_workflow_id
+	unset g_phase_encoding_dir
+
+	# parse arguments
+	local num_args=${#arguments[@]}
+	local argument
+	local index=0
+
+	while [ ${index} -lt ${num_args} ]; do
+		argument=${arguments[index]}
+
+		case ${argument} in
+			--help)
+				usage
+				exit 1
+				;;
+			--user=*)
+				g_user=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--password=*)
+				g_password=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--server=*)
+				g_server=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--project=*)
+				g_project=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--subject=*)
+				g_subject=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--session=*)
+				g_session=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--working-dir=*)
+				g_working_dir=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--workflow-id=*)
+				g_workflow_id=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--phase-encoding-dir=*)
+				g_phase_encoding_dir=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			*)
+				usage
+				echo "ERROR: unrecognized option: ${argument}"
+				echo ""
+				exit 1
+				;;
+		esac
+	done
+
+	local error_count=0
+
+	# check required parameters
+	if [ -z "${g_user}" ]; then
+		echo "ERROR: user (--user=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_user: ${g_user}"
+	fi
+
+	if [ -z "${g_password}" ]; then
+		echo "ERROR: password (--password=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_password: *******"
+	fi
+
+	if [ -z "${g_server}" ]; then
+		echo "ERROR: server (--server=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_server: ${g_server}"
+	fi
+
+	if [ -z "${g_project}" ]; then
+		echo "ERROR: project (--project=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_project: ${g_project}"
+	fi
+
+	if [ -z "${g_subject}" ]; then
+		echo "ERROR: subject (--subject=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_subject: ${g_subject}"
+	fi
+
+	if [ -z "${g_session}" ]; then
+		echo "ERROR: session (--session=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_session: ${g_session}"
+	fi
+
+	if [ -z "${g_working_dir}" ]; then
+		echo "ERROR: working directory (--working-dir=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_working_dir: ${g_working_dir}"
+	fi
+
+	if [ -z "${g_workflow_id}" ]; then
+		echo "ERROR: workflow ID (--workflow-id=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		echo "g_workflow_id: ${g_workflow_id}"
+	fi
+
+	if [ -z "${g_phase_encoding_dir}" ]; then
+		echo "ERROR: phase encoding dir specifier (--phase-encoding-dir=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		if [ "${g_phase_encoding_dir}" != "RLLR" ] ; then
+			if [ "${g_phase_encoding_dir}" != "PAAP" ] ; then
+				echo "ERROR: Unrecognized phase encoding dir specifier: ${g_phase_encoding_dir}"
+				error_count=$(( error_count + 1 ))
+			fi
+		fi
+	fi
+
+	if [ ${error_count} -gt 0 ]; then
+		echo "For usage information, use --help"
+		exit 1
+	fi
+}
+
+die()
+{
+	xnat_workflow_fail ${g_server} ${g_user} ${g_password} ${g_workflow_id}
+	exit 1
+}
+
+# Main processing
+#   Carry out the necessary steps to:
+#   - get prerequisite data for the Diffusion Preprocessing pipeline
+#   - run the DiffPreprocPipeline_PreEddy.sh script
+main()
+{
+	get_options $@
+
+	# Set up step counters
+	total_steps=10
+	current_step=0
+
+	xnat_workflow_show ${g_server} ${g_user} ${g_password} ${g_workflow_id}
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Link Structurally preprocessed data from DB
+	# ----------------------------------------------------------------------------------------------
+	current_step=$(( current_step + 1 ))
+	step_percent=$(( (current_step * 100) / total_steps ))
+
+	xnat_workflow_update ${g_server} ${g_user} ${g_password} ${g_workflow_id} \
+		${current_step} "Link Structurally preprocessed data from DB" ${step_percent}
+
+	link_hcp_struct_preproc_data "${DATABASE_ARCHIVE_ROOT}" "${g_project}" "${g_subject}" "${g_session}" "${g_working_dir}"
+
+	# ----------------------------------------------------------------------------------------------
+ 	# Step - Link unprocessed data from DB
+	# ----------------------------------------------------------------------------------------------
+	current_step=$(( current_step + 1 ))
+	step_percent=$(( (current_step * 100) / total_steps ))
+
+	xnat_workflow_update ${g_server} ${g_user} ${g_password} ${g_workflow_id} \
+		${current_step} "Link unprocessed data from DB" ${step_percent}
+
+	link_hcp_struct_unproc_data "${DATABASE_ARCHIVE_ROOT}" "${g_project}" "${g_subject}" "${g_session}" "${g_working_dir}"
+	link_hcp_diffusion_unproc_data "${DATABASE_ARCHIVE_ROOT}" "${g_project}" "${g_subject}" "${g_session}" "${g_working_dir}"
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Create a start_time file
+	# ----------------------------------------------------------------------------------------------
+	current_step=$(( current_step + 1 ))
+	step_percent=$(( (current_step * 100) / total_steps ))
+
+	xnat_workflow_update ${g_server} ${g_user} ${g_password} ${g_workflow_id} \
+		${current_step} "Create a start_time file" ${step_percent}
+	
+	start_time_file="${g_working_dir}/DiffusionPreproc.starttime"
+	if [ -e "${start_time_file}" ]; then
+		echo "Removing old ${start_time_file}"
+		rm -f ${start_time_file}
+	fi
+
+	# Sleep for 1 minute to make sure start_time file is created at least a
+	# minute after any files copied or linked above.
+	echo "Sleep for 1 minute before creating start_time file."
+	sleep 1m || die 
+	
+	echo "Creating start time file: ${start_time_file}"
+	touch ${start_time_file} || die 
+	ls -l ${start_time_file}
+
+	# Sleep for 1 minute to make sure any files created or modified by the scripts 
+	# are created at least 1 minute after the start_time file
+	echo "Sleep for 1 minute after creating start_time file."
+	sleep 1m || die 
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Set up to run DiffPreprocPipeline_PreEddy.sh script
+	# ----------------------------------------------------------------------------------------------
+	current_step=$(( current_step + 1 ))
+	step_percent=$(( (current_step * 100) / total_steps ))
+
+	xnat_workflow_update ${g_server} ${g_user} ${g_password} ${g_workflow_id} \
+		${current_step} "Set up to run DiffPreprocPipeline_PreEddy.sh script" ${step_percent}
+	
+	# Source setup script to setup environment for running the script
+
+	setup_file="${SCRIPTS_HOME}/SetUpHCPPipeline_DiffusionPreproc.sh"
+	if [ ! -e "${setup_file}" ] ; then
+		echo "setup_file: ${setup_file} DOES NOT EXIST - ABORTING"
+		die
+	fi
+
+	source "${setup_file}"
+
+	# ----------------------------------------------------------------------------------------------
+	# Step - Run the DiffPreprocPipeline_PreEddy.sh script
+	# ----------------------------------------------------------------------------------------------
+	current_step=$(( current_step + 1 ))
+	step_percent=$(( (current_step * 100) / total_steps ))
+
+	xnat_workflow_update ${g_server} ${g_user} ${g_password} ${g_workflow_id} \
+		${current_step} "Run the DiffPreprocPipeline_PreEddy.sh script" ${step_percent}
+
+	PreEddy_cmd=""
+	PreEddy_cmd+="${HCPPIPEDIR}/DiffusionPreprocessing/DiffPreprocPipeline_PreEddy.sh"
+ 	PreEddy_cmd+=" --path=${g_working_dir}"
+ 	PreEddy_cmd+=" --subject=${g_subject}"
+
+	if [ "${g_phase_encoding_dir}" = "RLLR" ] ; then
+		PreEddy_cmd+=" --PEdir=1"
+	elif [ "${g_phase_encoding_dir}" = "PAAP" ] ; then
+		PreEddy_cmd+=" --PEdir=2"
+	else
+		echo "ERROR: Unrecognized phase encoding dir specifier: ${g_phase_encoding_dir}"
+		exit 1
+	fi
+
+
+
+
+
+
+
+
+
+#  	PreFreeSurfer_cmd=""
+#  	PreFreeSurfer_cmd+="${HCPPIPEDIR}/PreFreeSurfer/PreFreeSurferPipeline.sh"
+#  	PreFreeSurfer_cmd+=" --path=${g_working_dir}"
+#  	PreFreeSurfer_cmd+=" --subject=${g_subject}"
+#  	PreFreeSurfer_cmd+=" --t1=${t1_spec}"
+#  	PreFreeSurfer_cmd+=" --t2=${t2_spec}"
+#  	PreFreeSurfer_cmd+=" --t1template=${HCPPIPEDIR}/global/templates/MNI152_T1_0.7mm.nii.gz"
+#  	PreFreeSurfer_cmd+=" --t1templatebrain=${HCPPIPEDIR}/global/templates/MNI152_T1_0.7mm_brain.nii.gz"
+#  	PreFreeSurfer_cmd+=" --t1template2mm=${HCPPIPEDIR}/global/templates/MNI152_T1_2mm.nii.gz"
+#  	PreFreeSurfer_cmd+=" --t2template=${HCPPIPEDIR}/global/templates/MNI152_T2_0.7mm.nii.gz"
+#  	PreFreeSurfer_cmd+=" --t2templatebrain=${HCPPIPEDIR}/global/templates/MNI152_T2_0.7mm_brain.nii.gz"
+#  	PreFreeSurfer_cmd+=" --t2template2mm=${HCPPIPEDIR}/global/templates/MNI152_T2_2mm.nii.gz"
+# 	PreFreeSurfer_cmd+=" --templatemask=${HCPPIPEDIR}/global/templates/MNI152_T1_0.7mm_brain_mask.nii.gz"
+# 	PreFreeSurfer_cmd+=" --template2mmmask=${HCPPIPEDIR}/global/templates/MNI152_T1_2mm_brain_mask_dil.nii.gz"
+# 	PreFreeSurfer_cmd+=" --fnirtconfig=${HCPPIPEDIR}/global/config/T1_2_MNI152_2mm.cnf"
+# 	PreFreeSurfer_cmd+=" --gdcoeffs=${HCPPIPEDIR}/global/config/coeff_SC72C_Skyra.grad"
+# 	PreFreeSurfer_cmd+=" --brainsize=150"
+# 	PreFreeSurfer_cmd+=" --echodiff=${g_first_t1w_deltaTE}"
+# 	PreFreeSurfer_cmd+=" --t1samplespacing=${g_first_t1w_sample_spacing}"
+# 	PreFreeSurfer_cmd+=" --t2samplespacing=${g_first_t2w_sample_spacing}"
+
+# 	if [[ ("${g_fieldmap_type}" == "GE") || ("${g_fieldmap_type}" == "SiemensGradientEcho") ]] ; then
+# 		# add parameters for Gradient Echo fieldmap usage
+# 		PreFreeSurfer_cmd+=" --avgrdcmethod=FIELDMAP"
+# 		PreFreeSurfer_cmd+=" --topupconfig=NONE"
+# 		PreFreeSurfer_cmd+=" --fmapmag=${g_working_dir}/${g_subject}/unprocessed/${TESLA_SPEC}/${FIRST_T1W_FILE_NAME_BASE}/${g_subject}_${TESLA_SPEC}_${MAG_FIELDMAP_NAME}${COMPRESSED_NIFTI_EXTENSION}"
+# 		PreFreeSurfer_cmd+=" --fmapphase=${g_working_dir}/${g_subject}/unprocessed/${TESLA_SPEC}/${FIRST_T1W_FILE_NAME_BASE}/${g_subject}_${TESLA_SPEC}_${PHASE_FIELDMAP_NAME}${COMPRESSED_NIFTI_EXTENSION}"
+# 		PreFreeSurfer_cmd+=" --unwarpdir=z"
+# 	elif [[ ("${g_fieldmap_type}" == "SE") || ("${g_fieldmap_type}" == "SpinEcho") ]] ; then
+# 		# add parameters for SpinEcho fieldmap usage
+# 		echo "SpinEcho fieldmaps UNHANDLED YET"
+# 		exit 1 
+# 	else
+# 		echo "ERROR: Unrecognized g_fieldmap_type: ${g_fieldmap_type}"
+# 		exit 1
+# 	fi
+
+	echo ""
+	echo "PreEddy_cmd: ${PreEddy_cmd}"
+	echo ""
+
+#	${PreEddy_cmd}
+	if [ $? -ne 0 ]; then
+		die 
+	fi
+
+
+}
+
+# Invoke the main function to get things started
+main $@
+
---
+++ DiffusionPreprocessingHCP/SubmitDiffusionPreprocessingHCP.OneSubject.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+# home directory for XNAT pipeline engine installation
+XNAT_PIPELINE_HOME=${HOME}/pipeline
+echo "XNAT_PIPELINE_HOME: ${XNAT_PIPELINE_HOME}"
+
+# home directory for XNAT utilities
+XNAT_UTILS_HOME=${HOME}/pipeline_tools/xnat_utilities
+echo "XNAT_UTILS_HOME: ${XNAT_UTILS_HOME}"
+
+# main build directory
+BUILD_HOME="/HCP/hcpdb/build_ssd/chpc/BUILD"
+echo "BUILD_HOME: ${BUILD_HOME}"
+
+get_options() 
+{
+	local arguments=($@)
+
+	# initialize global output variables
+	unset g_user
+	unset g_password
+	unset g_server
+	unset g_project
+	unset g_subject
+	unset g_session
+	unset g_phase_encoding_dir
+
+	# parse arguments
+	local num_args=${#arguments[@]}
+	local argument
+	local index=0
+
+	while [ ${index} -lt ${num_args} ]; do
+		argument=${arguments[index]}
+
+		case ${argument} in
+			--user=*)
+				g_user=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--password=*)
+				g_password=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--server=*)
+				g_server=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--project=*)
+				g_project=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--subject=*)
+				g_subject=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--session=*)
+				g_session=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--phase-encoding-dir=*)
+				g_phase_encoding_dir=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			*)
+				echo "ERROR: unrecognized option: ${argument}"
+				echo ""
+				exit 1
+				;;
+		esac
+	done
+
+	# set defaults and prompt for some unspecified parameters
+	if [ -z "${g_user}" ]; then
+		printf "Enter Connectome DB Username: "
+		read g_user
+	fi
+
+	if [ -z "${g_password}" ]; then
+		stty -echo
+		printf "Enter Connectome DB Password: "
+		read g_password
+		echo ""
+		stty echo
+	fi
+
+	if [ -z "${g_server}" ]; then
+		g_server="db.humanconnectome.org"
+	fi
+	echo "Connectome DB Server: ${g_server}"
+
+	if [ -z "${g_project}" ]; then
+		g_project="HCP_500"
+	fi
+    echo "Connectome DB Project: ${g_project}"
+
+	if [ -z "${g_subject}" ]; then
+		printf "Enter Connectome DB Subject: "
+		read g_subject
+	fi
+	echo "Connectome DB Subject: ${g_subject}"
+
+	if [ -z "${g_session}" ]; then
+		g_session=${g_subject}_3T
+	fi
+	echo "Connectome DB Session: ${g_session}"
+
+	if [ -z "${g_phase_encoding_dir}" ]; then
+		echo "ERROR: phase encoding dir specifier (--phase-encoding-dir=) required"
+		error_count=$(( error_count + 1 ))
+	else
+		if [ "${g_phase_encoding_dir}" != "RLLR" ] ; then
+			if [ "${g_phase_encoding_dir}" != "PAAP" ] ; then
+				echo "ERROR: Unrecognized phase encoding dir specifier: ${g_phase_encoding_dir}"
+				exit 1
+			fi
+		fi
+	fi
+}
+
+main()
+{
+	get_options $@
+
+	# Get token user id and password
+	echo "Setting up to run Python"
+	source ${SCRIPTS_HOME}/epd-python_setup.sh
+
+	echo "Getting token user id and password"
+	get_token_cmd="${XNAT_UTILS_HOME}/xnat_get_tokens --server=${g_server} --username=${g_user}"
+	#echo "get_token_cmd: ${get_token_cmd}"
+	get_token_cmd+=" --password=${g_password}"
+	new_tokens=`${get_token_cmd}`
+	token_username=${new_tokens% *}
+	token_password=${new_tokens#* }
+	echo "token_username: ${token_username}"
+	echo "token_password: ${token_password}"
+
+	unset pre_eddy_jobno
+
+	current_seconds_since_epoch=`date +%s`
+	working_directory_name="${BUILD_HOME}/${g_project}/DiffusionHCP.${current_seconds_since_epoch}_${g_subject}"
+
+	# Make the working directory
+	echo "Making working directory: ${working_directory_name}"
+	mkdir -p ${working_directory_name}
+
+	# Get JSESSION ID
+	echo "Getting JSESSION ID"
+	jsession=`curl -u ${g_user}:${g_password} https://db.humanconnectome.org/data/JSESSION`
+	echo "jsession: ${jsession}"
+
+	# Get XNAT Session ID (a.k.a. the experiment ID, e.g. ConnectomeDB_E1234)
+	echo "Getting XNAT Session ID"
+	get_session_id_cmd="python ${XNAT_PIPELINE_HOME}/catalog/ToolsHCP/resources/scripts/sessionid.py --server=db.humanconnectome.org --username=${g_user} --password=${g_password} --project=${g_project} --subject=${g_subject} --session=${g_session}"
+	#echo "get_session_id_cmd: ${get_session_id_cmd}"
+	sessionID=`${get_session_id_cmd}`
+	echo "XNAT session ID: ${sessionID}"
+
+	# Get XNAT Workflow ID
+	server="https://db.humanconnectome.org/"
+	echo "Getting XNAT workflow ID for this job from server: ${server}"
+	get_workflow_id_cmd="python ${XNAT_PIPELINE_HOME}/catalog/ToolsHCP/resources/scripts/workflow.py -User ${g_user} -Server ${server} -ExperimentID ${sessionID} -ProjectID ${g_project} -Pipeline RestingStateStats -Status Queued -JSESSION ${jsession}"
+	#echo "get_workflow_id_cmd: ${get_workflow_id_cmd}"
+	get_workflow_id_cmd+=" -Password ${g_password}"
+
+	workflowID=`${get_workflow_id_cmd}`
+	if [ $? -ne 0 ]; then
+		echo "Fetching workflow failed. Aborting"
+		echo "workflowID: ${workflowID}"
+		exit 1
+	elif [[ ${workflowID} == HTTP* ]]; then
+		echo "Fetching workflow failed. Aborting"
+		echo "worflowID: ${workflowID}"
+		exit 1
+	fi
+	echo "XNAT workflow ID: ${workflowID}"
+
+	# Submit job to do PreEddy work
+	pre_eddy_script_file_to_submit=${working_directory_name}/${g_subject}.DiffusionPreprocHCP.PreEddy.${g_project}.${g_session}.${current_seconds_since_epoch}.XNAT_PBS_job.sh
+	if [ -e "${pre_eddy_script_file_to_submit}" ]; then
+		rm -f "${pre_eddy_script_file_to_submit}"
+	fi
+
+	touch ${pre_eddy_script_file_to_submit}
+	echo "#PBS -l nodes=1:ppn=1,walltime=10:00:00,vmem=16000mb" >> ${pre_eddy_script_file_to_submit}
+	echo "#PBS -q dque" >> ${pre_eddy_script_file_to_submit}
+	echo "#PBS -o ${working_directory_name}" >> ${pre_eddy_script_file_to_submit}
+	echo "#PBS -e ${working_directory_name}" >> ${pre_eddy_script_file_to_submit}
+	echo ""
+	echo "/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/DiffusionPreprocessingHCP/DiffusionPreprocessingHCP_PreEddy.XNAT.sh \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --user=\"${token_username}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --password=\"${token_password}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --server=\"${g_server}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --project=\"${g_project}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --subject=\"${g_subject}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --session=\"${g_session}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --working-dir=\"${working_directory_name}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --workflow-id=\"${workflowID}\" \\" >> ${pre_eddy_script_file_to_submit}
+	echo "  --phase-encoding-dir=\"${g_phase_encoding_dir}\" " >> ${pre_eddy_script_file_to_submit}
+
+	chmod +x ${script_file_to_submit}
+
+	submit_cmd="qsub ${pre_eddy_script_file_to_submit}"
+	echo "submit_cmd: ${submit+cmd}"
+
+	pre_eddy_jobno=`${submit_cmd}`
+	echo "pre_eddy_jobno: ${pre_eddy_jobno}"
+
+
+	
+
+
+
+
+
+
+
+
+}
+
+# Invoke the main function to get things started
+main $@
