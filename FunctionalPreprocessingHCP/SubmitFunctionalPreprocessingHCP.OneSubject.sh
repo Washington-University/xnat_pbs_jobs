0e292fcc59940201fd89ab4927ed7b19dd6297c1
Timothy B. Brown <tbbrown@wustl.edu>
2016-02-10 16:49:38 -0600

    Corrected resource name and added option to suppress putting data in DB to SubmitFunctionalPrepreocessingHCP.OneSubject.sh

--- FunctionalPreprocessingHCP/SubmitFunctionalPreprocessingHCP.OneSubject.sh
+++ FunctionalPreprocessingHCP/SubmitFunctionalPreprocessingHCP.OneSubject.sh
@@ -39,6 +39,7 @@ get_options()
 	unset g_subject
 	unset g_session
 	unset g_put_server
+	unset g_suppress_put
 
 	# parse arguments
 	local num_args=${#arguments[@]}
@@ -75,6 +76,10 @@ get_options()
 				;;
 			--put-server=*)
 				g_put_server=${argument/*=/""}
+				index=$(( index + 1 ))
+				;;
+			--suppress-put*)
+				g_suppress_put="TRUE"
 				index=$(( index + 1 ))
 				;;
 			*)
@@ -124,6 +129,11 @@ get_options()
 		g_put_server="db.humanconnectome.org"
 	fi
 	echo "PUT server: ${g_put_server}"
+
+	if [ -z "${g_suppress_put}" ]; then
+		g_suppress_put="FALSE"
+	fi
+	echo "Suppress PUT: ${g_suppress_put}"
 }
 
 main()
@@ -318,19 +328,23 @@ main()
  			echo "  --subject=\"${g_subject}\" \\" >> ${put_script_file_to_submit}
  			echo "  --session=\"${g_session}\" \\" >> ${put_script_file_to_submit}
  			echo "  --working-dir=\"${working_directory_name}\" \\" >> ${put_script_file_to_submit}
-			echo "  --resource-suffix=\"${scan}_preproc_TEST\" \\" >> ${put_script_file_to_submit}
+			echo "  --resource-suffix=\"${scan}_preproc\" \\" >> ${put_script_file_to_submit}
 			echo "  --reason=\"${scan}_FunctionalPreprocessingHCP\" " >> ${put_script_file_to_submit}
 
 			chmod +x ${put_script_file_to_submit}
 
-			submit_cmd="qsub -W depend=afterok:${processing_job_no} ${put_script_file_to_submit}"
-			echo "submit_cmd: ${submit_cmd}"
-			${submit_cmd}
+			put_submit_cmd="qsub -W depend=afterok:${processing_job_no} ${put_script_file_to_submit}"
+			echo "put_submit_cmd: ${put_submit_cmd}"
+
+			if [ "${g_suppress_put}" = "TRUE" ] ; then
+				echo "PUT operation has been suppressed. Resource will not be put in DB. Working Directory will not be deleted."
+			else
+				${put_submit_cmd}
+			fi
 
 		done # postfix in RL LR
 
 	done # scan_name in ${resting_state_scan_names}
-
 }
 
 # Invoke the main function to get things started
