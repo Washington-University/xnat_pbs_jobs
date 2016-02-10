b1c9fb057bf49a1f371c77cbe894985638f85781
Timothy B. Brown <tbbrown@wustl.edu>
2016-02-10 16:55:55 -0600

    Working on diffusion packaging

---
+++ DiffusionPreprocessingHCP/SubmitDiffusionPackagingHCP.HCP_Staging.Batch.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ -z "${SUBJECT_FILES_DIR}" ]; then
+	echo "Environment variable SUBJECT_FILES_DIR must be set!"
+	exit 1
+fi
+
+printf "Connectome DB Username: "
+read userid
+
+stty -echo
+printf "Connectome DB Password: "
+read password
+echo ""
+stty echo
+
+project="HCP_Staging"
+subject_file_name="${SUBJECT_FILES_DIR}/${project}.DiffusionPackagingHCP.subjects"
+echo "Retrieving subject list from: ${subject_file_name}"
+subject_list_from_file=( $( cat ${subject_file_name} ) )
+subjects="`echo "${subject_list_from_file[@]}"`"
+
+start_shadow_number=1
+max_shadow_number=8
+
+shadow_number=${start_shadow_number}
+
+for subject in ${subjects} ; do
+
+	if [[ ${subject} != \#* ]]; then
+
+		server="db-shadow${shadow_number}.nrg.mir"
+
+ 		echo ""
+		echo "--------------------------------------------------------------------------------"
+		echo " Submitting Diffusion Packaging job for subject: ${subject}"
+		echo "--------------------------------------------------------------------------------"
+
+		${HOME}/pipeline_tools/xnat_pbs_jobs/DiffusionPreprocessingHCP/SubmitDiffusionPackagingHCP.OneSubject.sh \
+			--user=${userid} \
+			--password=${password} \
+			--server=${server} \
+			--project=${project} \
+			--subject=${subject} 
+
+		shadow_number=$((shadow_number+1))
+		if [ "${shadow_number}" -gt "${max_shadow_number}" ]; then
+			shadow_number=${start_shadow_number}
+		fi
+
+	fi
+	
+done
