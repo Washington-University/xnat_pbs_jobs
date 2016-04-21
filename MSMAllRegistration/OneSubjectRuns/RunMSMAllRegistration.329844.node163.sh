718c55323136485072f3c722ca3f5f8664f797b7
Timothy B. Brown <tbbrown@wustl.edu>
2016-04-21 15:06:05 -0500

    Changes to run MSMAllRegistration without a job scheduler

---
+++ MSMAllRegistration/OneSubjectRuns/RunMSMAllRegistration.329844.node163.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+printf "Connectome DB Username: "
+read userid
+
+stty -echo
+printf "Connectome DB Password: "
+read password
+echo ""
+stty echo
+
+subject="329844"
+node="node163"
+shadow_number=7
+
+project="HCP_Staging"
+server="db-shadow${shadow_number}.nrg.mir:8080"
+
+echo ""
+echo "--------------------------------------------------------------------------------"
+echo " Running MSMAllRegistration job for subject: ${subject}"
+echo " In project: ${project}"
+echo " Using server: ${server}"
+echo " Using compute node: ${node}"
+echo "--------------------------------------------------------------------------------"
+
+
+at now <<EOF
+
+${HOME}/pipeline_tools/xnat_pbs_jobs/MSMAllRegistration/RunMSMAllRegistration.OneSubject.sh \
+	--user=${userid} \
+	--password=${password} \
+	--server=${server} \
+	--project=${project} \
+	--subject=${subject} \
+	--node=${node} \
+	> /home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/MSMAllRegistration/OneSubjectRuns/${subject}.${node}.stdout \
+	2>/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/MSMAllRegistration/OneSubjectRuns/${subject}.${node}.stderr
+EOF
--- StructuralPreprocessingHCP/OneSubjectRuns/RunStructuralPreprocessingHCP.HCP_Staging.971160.Seed5678.test_resource.Pipelines_dev.sh
+++ StructuralPreprocessingHCP/OneSubjectRuns/RunStructuralPreprocessingHCP.HCP_Staging.971160.Seed5678.test_resource.Pipelines_dev.sh
@@ -12,7 +12,8 @@ stty echo
 seed=5678
 subject="971160"
 shadow_number=1
-node="node017"
+#node="node017"
+node="node100"
 
 project="HCP_Staging"
 server="db-shadow${shadow_number}.nrg.mir:8080"
@@ -34,7 +35,8 @@ at now <<EOF
 	--seed=${seed} \
 	--node=${node} \
 	--output-resource="Structural_preproc_test_Pipelines_dev" \
-	--setup-script="/home/HCPpipeline/SCRIPTS/SetUpHCPPipeline_StructuralPreprocHCP.Pipelines_dev.sh"
-
+	--setup-script="/home/HCPpipeline/SCRIPTS/SetUpHCPPipeline_StructuralPreprocHCP.Pipelines_dev.sh" \
+	> /home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/StructuralPreprocessingHCP/OneSubjectRuns/${subject}.${seed}.${node}.stdout \
+	2>/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/StructuralPreprocessingHCP/OneSubjectRuns/${subject}.${seed}.${node}.stderr
 EOF
 
