f8685d76069cc3f8a4a35737cd46d850e8ae817f
Timothy B. Brown <tbbrown@wustl.edu>
2017-02-27 15:55:44 -0600

    Moved some DeDriftAndResample subject files into directory with scripts

--- DeDriftAndResample/SubmitDeDriftAndResample.HCP_900.Batch.sh
+++ DeDriftAndResample/SubmitDeDriftAndResample.HCP_900.Batch.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ -z "${SUBJECT_FILES_DIR}" ]; then
-	echo "Environment variable SUBJECT_FILES_DIR must be set!"
-	exit 1
-fi
-
 printf "Connectome DB Username: "
 read userid
 
@@ -14,27 +9,13 @@ read password
 echo ""
 stty echo
 
-printf "Delay until first submission (minutes) [0]: "
-read delay
-
-if [ -z "${delay}" ]; then
-	delay=0
-fi
-
-printf "Interval between submissions (minutes) [60]: "
-read interval
-
-if [ -z "${interval}" ]; then
-	interval=60
-fi
-
 project="HCP_900"
-subject_file_name="${SUBJECT_FILES_DIR}/${project}.DeDriftAndResample.subjects"
+subject_file_name="${project}.DeDriftAndResample.subjects"
 echo "Retrieving subject list from: ${subject_file_name}"
 subject_list_from_file=( $( cat ${subject_file_name} ) )
 subjects="`echo "${subject_list_from_file[@]}"`"
 
-start_shadow_number=1
+start_shadow_number=6
 max_shadow_number=8
 
 shadow_number=${start_shadow_number}
@@ -52,16 +33,12 @@ for subject in ${subjects} ; do
 		echo " Submission delayed until ${delay} minutes from now"
 		echo "--------------------------------------------------------------------------------"
 		
-		at now + ${delay} minutes <<EOF 
-			/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/DeDriftAndResample/SubmitDeDriftAndResample.OneSubject.sh \
+		/home/HCPpipeline/pipeline_tools/xnat_pbs_jobs/DeDriftAndResample/SubmitDeDriftAndResample.OneSubject.sh \
 			--user=${userid} \
 			--password=${password} \
 			--server=${server} \
 			--project=${project} \
 			--subject=${subject}
-EOF
-
-		delay=$((delay + interval))
 
 		shadow_number=$((shadow_number+1))
 		
