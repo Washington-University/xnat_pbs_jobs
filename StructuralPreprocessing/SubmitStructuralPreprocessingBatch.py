12a1b8a246f676cbab3864843c83b80a260f7a6a
Timothy B. Brown <tbbrown@wustl.edu>
2017-05-05 09:59:24 -0500

    Working on Structure Preprocessing for CCF/LifeSpan

---
+++ StructuralPreprocessing/SubmitStructuralPreprocessingBatch.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# import of built-in modules
+import getpass
+import logging
+import logging.config
+import os
+
+# import of third-party modules
+
+# import of local modules
+import ccf.archive as ccf_archive
+import ccf.batch_submitter as batch_submitter
+import ccf.structural_preprocessing.one_subject_job_submitter as one_subject_job_submitter
+import ccf.subject as ccf_subject
+import utils.file_utils as file_utils
+import utils.my_configparser as my_configparser
+
+# authorship information
+__author__ = "Timothy B. Brown"
+__copyright__ = "Copyright 2017, The Human Connectome Project/Connectome Coordination Facility"
+__maintainer__ = "Timothy B. Brown"
+
+# configure logging and create a module logger
+module_logger = logging.getLogger(file_utils.get_logger_name(__file__))
+module_logger.setLevel(logging.WARNING)  # Note: This can be overridden by file configuration
+
+
+class BatchSubmitter(batch_submitter.BatchSubmitter):
+
+	def __init__(self, tesla_spec):
+		super().__init__(ccf_archive.CcfArchive(tesla_spec))
+
+	def submit_jobs(self, username, password, subject_list, config):
+
+		# submit jobs for the listed subjects
+		for subject in subject_list:
+
+			submitter = one_subject_job_submitter.OneSubjectJobSubmitter(
+				self._archive, self._archive.build_home)
+
+			put_server = 'http://db-shadow' + str(self.get_and_inc_shadow_number()) + '.nrg.mir:8080'
+
+			# get information for the subject from the configuration
+			setup_file = config.get_value(subject.subject_id, 'SetUpFile')
+			clean_output_first = config.get_bool_value(subject.subject_id, 'CleanOutputFirst')
+			processing_stage_str = config.get_value(subject.subject_id, 'ProcessingStage')
+			processing_stage = submitter.processing_stage_from_string(processing_stage_str)
+			walltime_limit_hrs = config.get_value(subject.subject_id, 'WalltimeLimitHours')
+			vmem_limit_gbs = config.get_value(subject.subject_id, 'VmemLimitGbs')
+			output_resource_suffix = config.get_value(subject.subject_id, 'OutputResourceSuffix')
+			
+			module_logger.info("-----")
+			module_logger.info(" Submitting " + submitter.PIPELINE_NAME + " jobs for:")
+			module_logger.info("                project: " + subject.project)
+			module_logger.info("                subject: " + subject.subject_id)
+			module_logger.info("             put_server: " + put_server)
+			module_logger.info("             setup_file: " + setup_file)
+			module_logger.info("     clean_output_first: " + str(clean_output_first))
+			module_logger.info("       processing_stage: " + str(processing_stage))
+			module_logger.info("     walltime_limit_hrs: " + str(walltime_limit_hrs))
+			module_logger.info("         vmem_limit_gbs: " + str(vmem_limit_gbs))
+			module_logger.info(" output_resource_suffix: " + output_resource_suffix)
+			module_logger.info("-----")
+
+			# user and server information
+			submitter.username = username
+			submitter.password = password
+			submitter.server = 'https://db.humanconnectome.org'
+
+			# subject and project information
+			submitter.project = subject.project
+			submitter.subject = subject.subject_id
+			submitter.session = subject.subject_id + '_3T'
+
+			# job parameters
+			submitter.setup_script = setup_file
+			submitter.clean_output_resource_first = clean_output_first
+			submitter.put_server = put_server
+			submitter.walltime_limit_hours = walltime_limit_hrs
+			submitter.vmem_limit_gbs = vmem_limit_gbs
+			submitter.output_resource_suffix = output_resource_suffix
+
+			# submit jobs
+			submitter.submit_jobs(processing_stage)
+			
+if __name__ == '__main__':
+	logging.config.fileConfig(
+		file_utils.get_logging_config_file_name(__file__),
+		disable_existing_loggers=False)
+	
+	# get ConnectomeDB credentials
+	userid = input("Connectome DB Username: ")
+	password = getpass.getpass("Connectome DB Password: ")
+
+	# read the configuration file
+	config_file_name = file_utils.get_config_file_name(__file__)
+	module_logger.info("Reading configuration from file: " + config_file_name)
+	config = my_configparser.MyConfigParser()
+	config.read(config_file_name)
+	
+	# get list of subjects to process
+	subject_file_name = 'subjectfiles' + os.sep + file_utils.get_subjects_file_name(__file__)
+	module_logger.info("Retrieving subject list from: " + subject_file_name)
+	subject_list = ccf_subject.read_subject_info_list(subject_file_name, separator=":")
+
+	# process the subjects in the list
+	batch_submitter = BatchSubmitter('3T')
+	batch_submitter.submit_jobs(userid, password, subject_list, config)
---
+++ lib/ccf/structural_preprocessing/one_subject_job_submitter.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+# import of built-in modules
+import contextlib
+import logging
+import os
+import stat
+
+# import of third-party modules
+
+# import of local modules
+import ccf.one_subject_job_submitter as one_subject_job_submitter
+import utils.debug_utils as debug_utils
+import utils.str_utils as str_utils
+
+# authorship information
+__author__ = "Timothy B. Brown"
+__copyright__ = "Copyright 2017, Connectome Coordination Facility"
+__maintainer__ = "Timothy B. Brown"
+
+# create a module logger
+module_logger = logging.getLogger(__name__)
+module_logger.setLevel(logging.WARNING)  # Note: This can be overidden by log file configuration
+
+
+class OneSubjectJobSubmitter(one_subject_job_submitter.OneSubjectJobSubmitter):
+
+	def __init__(self, archive, build_home):
+		super().__init__(archive, build_home)
+
+	@property
+	def PIPELINE_NAME(self):
+		return "StructuralPreprocessing"
+
+	@property
+	def WORK_NODE_COUNT(self):
+		return 1
+
+	@property
+	def WORK_PPN(self):
+		return 1
+
+	def create_work_script(self):
+		module_logger.debug(debug_utils.get_name())
+
+		script_name = self.work_script_name
+
+		with contextlib.suppress(FileNotFoundError):
+			os.remove(script_name)
+			
+		walltime_limit_str = str(self.walltime_limit_hours) + ':00:00'
+		vmem_limit_str = str(self.vmem_limit_gbs) + 'gb'
+
+		resources_line = '#PBS -l nodes=' + str(self.WORK_NODE_COUNT)
+		resources_line += ':ppn=' + str(self.WORK_PPN)
+		resources_line += ',walltime=' + walltime_limit_str
+		resources_line += ',vmem=' + vmem_limit_str
+		
+		stdout_line = '#PBS -o ' + self.working_directory_name
+		stderr_line = '#PBS -e ' + self.working_directory_name
+
+		script_line = self.xnat_pbs_jobs_home + os.sep
+		script_line += self.PIPELINE_NAME + os.sep + self.PIPELINE_NAME + '.XNAT.sh'
+
+		user_line = '  --user=' + self.username
+		password_line = '  --password=' + self.password
+		server_line = '  --server=' + str_utils.get_server_name(self.server)
+		project_line = '  --project=' + self.project
+		subject_line = '  --subject=' + self.subject
+		session_line = '  --session=' + self.session
+		wdir_line = '  --working-dir=' + self.working_directory_name
+		setup_line = '  --setup-script=' + self.xnat_pbs_jobs_home + os.sep + self.PIPELINE_NAME + os.sep + self.setup_script
+		
+		with open(script_name, 'w') as script:
+			script.write(resources_line + os.linesep)
+			script.write(stdout_line + os.linesep)
+			script.write(stderr_line + os.linesep)
+			script.write(os.linesep)
+			script.write(script_line + ' \\' + os.linesep)
+			script.write(user_line + ' \\' + os.linesep)
+			script.write(password_line + ' \\' + os.linesep)
+			script.write(server_line + ' \\' + os.linesep)
+			script.write(project_line + ' \\' + os.linesep)
+			script.write(subject_line + ' \\' + os.linesep)
+			script.write(session_line + ' \\' + os.linesep)
+			script.write(wdir_line + '\\' + os.linesep)
+			script.write(setup_line + os.linesep)
+
+			os.chmod(script_name, stat.S_IRWXU | stat.S_IRWXG)
+
+	def output_resource_name(self):
+		module_logger.debug(debug_utils.get_name())
+		return self.output_resource_suffix
+	
