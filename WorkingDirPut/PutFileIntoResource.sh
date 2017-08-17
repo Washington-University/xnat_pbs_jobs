d811f83aa763cd44baaab000f74eccfc57e06aad
Timothy B. Brown <tbbrown@wustl.edu>
2017-08-17 14:09:09 -0500

    Fixed problem with documentation in PutFileIntoResource.sh script

--- WorkingDirPut/PutFileIntoResource.sh
+++ WorkingDirPut/PutFileIntoResource.sh
@@ -27,29 +27,29 @@ Overwrites the file, but does not replace the entire resource.
 
 Example invocation when file is available on the server
 
-  ./PutFileIntoResource.sh \
-    --user=tbbrown \
-    --password=<some_password> \
-    --project=PipelineTest \
-    --subject=100307 \
-    --session=100307_3T \
-    --resource=Structural_preproc \
-    --file=/data/hcpdb/build_ssd/chpc/BUILD/PipelineTest/test/T1w/wmparc.nii.gz \   # Notice that the file is available on the server at this specified path
-    --file-path-within-resource=T1w/wmparc.nii.gz  \                                # Should not start with a slash "/"
+  ./PutFileIntoResource.sh 
+    --user=tbbrown 
+    --password=<some_password> 
+    --project=PipelineTest 
+    --subject=100307 
+    --session=100307_3T 
+    --resource=Structural_preproc 
+    --file=/data/hcpdb/build_ssd/chpc/BUILD/PipelineTest/test/T1w/wmparc.nii.gz   # Notice that the file is available on the server at this specified path
+    --file-path-within-resource=T1w/wmparc.nii.gz                                 # Should not start with a slash "/"
     --force
 
 Example invocation when file is not availble on the server
 
-  ./PutFileIntoResource.sh \
-    --user=tbbrown \
-    --password=<some_password> \
-    --project=PipelineTest \
-    --subject=100307 \
-    --session=100307_3T \
-    --resource=Structural_preproc \
-    --file=/HCP/hcpdb/build_ssd/this_file \          # Notice that the file is NOT available on the server
-    --file-path-within-resource=T1w/wmparc.nii.gz \  # Should not start with a slash "/"
-    --force \
+  ./PutFileIntoResource.sh 
+    --user=tbbrown 
+    --password=<some_password> 
+    --project=PipelineTest 
+    --subject=100307 
+    --session=100307_3T 
+    --resource=Structural_preproc 
+    --file=/HCP/hcpdb/build_ssd/this_file           # Notice that the file is NOT available on the server
+    --file-path-within-resource=T1w/wmparc.nii.gz   # Should not start with a slash "/"
+    --force 
     --use-http
 
 EOF
@@ -72,26 +72,26 @@ get_options()
     unset g_file
     unset g_file_path_within_resource # should not start with a slash
     unset g_force
-        unset g_use_http
-
-        # default values
-        g_use_http="FALSE"
-        g_reason="Unspecified"
-        g_force="FALSE"
-
+    unset g_use_http
+	
+    # default values
+    g_use_http="FALSE"
+    g_reason="Unspecified"
+    g_force="FALSE"
+	
     # parse arguments
     local num_args=${#arguments[@]}
     local argument
     local index=0
-
+	
     while [ ${index} -lt ${num_args} ]; do
         argument=${arguments[index]}
-
+		
         case ${argument} in
-                        --help)
-                                usage
-                                exit 1
-                                ;;
+            --help)
+                usage
+                exit 1
+                ;;
             --user=*)
                 g_user=${argument/*=/""}
                 index=$(( index + 1 ))
@@ -140,21 +140,21 @@ get_options()
                 g_file_path_within_resource=${argument/*=/""}
                 index=$(( index + 1 ))
                 ;;
-                        --use-http)
-                                g_use_http="TRUE"
-                                index=$(( index + 1 ))
-                                ;;
+            --use-http)
+                g_use_http="TRUE"
+                index=$(( index + 1 ))
+                ;;
             *)
                 usage
-                                log_Err_Abort "unrecognized option: ${argument}"
+                log_Err_Abort "unrecognized option: ${argument}"
                 ;;
         esac
     done
-
+	
     local default_server="${XNAT_PBS_JOBS_XNAT_SERVER}"
-
+	
     local error_count=0
-
+	
     # check parameters
     if [ -z "${g_user}" ]; then
         log_Err "user (--user=) required"
@@ -162,18 +162,18 @@ get_options()
     else
         log_Msg "g_user: ${g_user}"
     fi
-
+	
     if [ -z "${g_password}" ]; then
         log_Err "password (--password=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_password: *******"
     fi
-
+	
     if [ -z "${g_server}" ]; then
         g_server="${default_server}"
     fi
-
+	
     if [ -z "${g_protocol}" ]; then
         if [ "${g_server}" = "${default_server}" ]; then
             g_protocol="https"
@@ -181,69 +181,69 @@ get_options()
             g_protocol="http"
         fi
     fi
-
-        if [ "${g_protocol}" != "https" -a "${g_protocol}" != "http" ]; then
-                log_Err "Unrecognized protocol: ${g_protocol}"
-                error_count=$(( error_count + 1 ))
-        fi
-
-        log_Msg "g_protocol: ${g_protocol}"
+	
+    if [ "${g_protocol}" != "https" -a "${g_protocol}" != "http" ]; then
+        log_Err "Unrecognized protocol: ${g_protocol}"
+        error_count=$(( error_count + 1 ))
+    fi
+	
+    log_Msg "g_protocol: ${g_protocol}"
     log_Msg "g_server: ${g_server}"
-
+	
     if [ -z "${g_project}" ]; then
         log_Err "project (--project=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_project: ${g_project}"
     fi
-
+	
     if [ -z "${g_subject}" ]; then
         log_Err "subject (--subject=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_subject: ${g_subject}"
     fi
-
-        if [ -z "${g_session}" ]; then
+	
+    if [ -z "${g_session}" ]; then
         log_Err "session (--session=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_session: ${g_session}"
     fi
-
+	
     if [ -z "${g_resource}" ]; then
         log_Err "resource (--resource=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_resource: ${g_resource}"
     fi
-
+	
     if [ -z "${g_file}" ]; then
         log_Err "file (--file=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_file: ${g_file}"
     fi
-
+	
     if [ -z "${g_file_path_within_resource}" ]; then
         log_Err "file path within resource (--file-path-within-resource=) required"
         error_count=$(( error_count + 1 ))
     else
         log_Msg "g_file_path_within_resource: ${g_file_path_within_resource}"
     fi
-
-        if [ -z "${g_reason}" ]; then
-                log_Err "reason (--reason=) required"
-                error_count=$(( error_count + 1 ))
-        else
-                log_Msg "g_reason: ${g_reason}"
-        fi
-
+	
+    if [ -z "${g_reason}" ]; then
+        log_Err "reason (--reason=) required"
+        error_count=$(( error_count + 1 ))
+    else
+        log_Msg "g_reason: ${g_reason}"
+    fi
+	
     log_Msg "g_force: ${g_force}"
-        log_Msg "g_use_http: ${g_use_http}"
-
+    log_Msg "g_use_http: ${g_use_http}"
+	
     if [ ${error_count} -gt 0 ]; then
-                usage
+        usage
         exit 1
     fi
 }
@@ -263,7 +263,7 @@ utils_IsYes() {
 utils_ShouldProceed() {
     echo -ne "Proceed? [n]: "
     read proceed
-
+	
     if utils_IsYes $proceed
     then
         return 0 # Should proceed
@@ -275,116 +275,116 @@ utils_ShouldProceed() {
 main()
 {
     get_options "$@"
-
-        data_client_jar="${XNAT_PBS_JOBS_PIPELINE_ENGINE}/lib/xnat-data-client-1.6.4-SNAPSHOT-jar-with-dependencies.jar"
-        get_session_id_script="${XNAT_PBS_JOBS_PIPELINE_ENGINE}/catalog/ToolsHCP/resources/scripts/sessionid.py"
-
-        # Set up to run Python
+	
+    data_client_jar="${XNAT_PBS_JOBS_PIPELINE_ENGINE}/lib/xnat-data-client-1.6.4-SNAPSHOT-jar-with-dependencies.jar"
+    get_session_id_script="${XNAT_PBS_JOBS_PIPELINE_ENGINE}/catalog/ToolsHCP/resources/scripts/sessionid.py"
+	
+    # Set up to run Python
     source ${XNAT_PBS_JOBS}/ToolSetupScripts/epd-python_setup.sh
-
+	
     # Get XNAT Session ID (a.k.a. the experiment ID, e.g ConnectomeDB_E1234)
     get_session_id_cmd="python ${get_session_id_script}"
-        get_session_id_cmd+=" --server=${XNAT_PBS_JOBS_XNAT_SERVER}"
-        get_session_id_cmd+=" --username=${g_user}"
-        get_session_id_cmd+=" --password=${g_password}"
-        get_session_id_cmd+=" --project=${g_project}"
-        get_session_id_cmd+=" --subject=${g_subject}"
-        get_session_id_cmd+=" --session=${g_session}"
-        # Since this command contains a password, it should only be logged in debugging mode.
+    get_session_id_cmd+=" --server=${XNAT_PBS_JOBS_XNAT_SERVER}"
+    get_session_id_cmd+=" --username=${g_user}"
+    get_session_id_cmd+=" --password=${g_password}"
+    get_session_id_cmd+=" --project=${g_project}"
+    get_session_id_cmd+=" --subject=${g_subject}"
+    get_session_id_cmd+=" --session=${g_session}"
+    # Since this command contains a password, it should only be logged in debugging mode.
     #log_Msg "get_session_id_cmd: ${get_session_id_cmd}"
     sessionID=$(${get_session_id_cmd})
-    inform "XNAT session ID: ${sessionID}"
-
-        if [ "${g_use_http}" = "TRUE" ]; then
-                resource_url=""
-                resource_url+="${g_protocol}:"
-                resource_url+="//${g_server}"
-                resource_url+="/REST/projects/${g_project}"
-                resource_url+="/subjects/${g_subject}"
-                resource_url+="/experiments/${sessionID}"
-                resource_url+="/resources/${g_resource}"
-                resource_url+="/files"
-                resource_url+="/${g_file_path_within_resource}"
-
-                variable_values=""
-                variable_values+="?overwrite=true"
-                variable_values+="&replace=true"
-                variable_values+="&event_reason=${g_reason}"
-
-                resource_uri="${resource_url}${variable_values}"
-                log_Msg "resource_uri: ${resource_uri}"
-
-                if [ "${g_force}" = "TRUE" ]; then
-                        put_it="TRUE"
-                elif utils_ShouldProceed ; then
-                        put_it="TRUE"
-                else
-                        put_it="FALSE"
-                fi
-
-                if [ "${put_it}" = "TRUE" ]; then
-                        java_cmd=""
-                        java_cmd+="java -Xmx1024m -jar ${data_client_jar}"
-                        java_cmd+=" -u ${g_user}"
-                        java_cmd+=" -p ${g_password}"
-                        java_cmd+=" -m PUT"
-                        java_cmd+=" -r ${resource_uri}"
-                        java_cmd+=" -l ${g_file}"
-
-                        log_Msg "Using java -Xmx1024m -jar ${data_client_jar} to PUT the file: ${g_file} into the resource: ${resource_uri}"
-                        ${java_cmd}
-
-                else
-                        log_Msg "Did not attempt to put to resource: ${resource_uri}"
-
-                fi
-
-        else
-                # The specified file path is available on the server, so upload it "by reference"
-                resource_url=""
-                resource_url+="${g_protocol}:"
-                resource_url+="//${g_server}"
-                resource_url+="/REST/projects/${g_project}"
-                resource_url+="/subjects/${g_subject}"
-                resource_url+="/experiments/${sessionID}"
-                resource_url+="/resources/${g_resource}"
-                resource_url+="/files"
-                resource_url+="/${g_file_path_within_resource}"
-
-                variable_values=""
-                variable_values+="?overwrite=true"
-                variable_values+="&replace=true"
+    log_Msg "XNAT session ID: ${sessionID}"
+	
+    if [ "${g_use_http}" = "TRUE" ]; then
+        resource_url=""
+        resource_url+="${g_protocol}:"
+        resource_url+="//${g_server}"
+        resource_url+="/REST/projects/${g_project}"
+        resource_url+="/subjects/${g_subject}"
+        resource_url+="/experiments/${sessionID}"
+        resource_url+="/resources/${g_resource}"
+        resource_url+="/files"
+        resource_url+="/${g_file_path_within_resource}"
+		
+        variable_values=""
+        variable_values+="?overwrite=true"
+        variable_values+="&replace=true"
         variable_values+="&event_reason=${g_reason}"
-                variable_values+="&reference=${g_file}"
-
-                resource_uri="${resource_url}${variable_values}"
-                log_Msg "resource_uri: ${resource_uri}"
-
-                if [ ! -z "${g_force}" ]; then
-                        put_it="TRUE"
-                elif utils_ShouldProceed ; then
-                        put_it="TRUE"
-                else
-                        put_it="FALSE"
-                fi
-
-                if [ "${put_it}" = "TRUE" ]; then
-                        java_cmd=""
-                        java_cmd+="java -Xmx1024m -jar ${data_client_jar}"
-                        java_cmd+=" -u ${g_user}"
-                        java_cmd+=" -p ${g_password}"
-                        java_cmd+=" -m PUT"
-                        java_cmd+=" -r ${resource_uri}"
-
-                        log_Msg "Using java -Xmx1024m -jar ${data_client_jar} to PUT the resource: ${resource_uri}"
-                        ${java_cmd}
-
-                else
-                        log_Msg "Did not attempt to put to resource: ${resource_url}"
-
-                fi
-
+		
+        resource_uri="${resource_url}${variable_values}"
+        log_Msg "resource_uri: ${resource_uri}"
+		
+        if [ "${g_force}" = "TRUE" ]; then
+            put_it="TRUE"
+        elif utils_ShouldProceed ; then
+            put_it="TRUE"
+        else
+            put_it="FALSE"
         fi
+		
+        if [ "${put_it}" = "TRUE" ]; then
+            java_cmd=""
+            java_cmd+="java -Xmx1024m -jar ${data_client_jar}"
+            java_cmd+=" -u ${g_user}"
+            java_cmd+=" -p ${g_password}"
+            java_cmd+=" -m PUT"
+            java_cmd+=" -r ${resource_uri}"
+            java_cmd+=" -l ${g_file}"
+			
+            log_Msg "Using java -Xmx1024m -jar ${data_client_jar} to PUT the file: ${g_file} into the resource: ${resource_uri}"
+            ${java_cmd}
+			
+        else
+            log_Msg "Did not attempt to put to resource: ${resource_uri}"
+			
+        fi
+		
+    else
+        # The specified file path is available on the server, so upload it "by reference"
+        resource_url=""
+        resource_url+="${g_protocol}:"
+        resource_url+="//${g_server}"
+        resource_url+="/REST/projects/${g_project}"
+        resource_url+="/subjects/${g_subject}"
+        resource_url+="/experiments/${sessionID}"
+        resource_url+="/resources/${g_resource}"
+        resource_url+="/files"
+        resource_url+="/${g_file_path_within_resource}"
+		
+        variable_values=""
+        variable_values+="?overwrite=true"
+        variable_values+="&replace=true"
+        variable_values+="&event_reason=${g_reason}"
+        variable_values+="&reference=${g_file}"
+		
+        resource_uri="${resource_url}${variable_values}"
+        log_Msg "resource_uri: ${resource_uri}"
+		
+        if [ ! -z "${g_force}" ]; then
+            put_it="TRUE"
+        elif utils_ShouldProceed ; then
+            put_it="TRUE"
+        else
+            put_it="FALSE"
+        fi
+		
+        if [ "${put_it}" = "TRUE" ]; then
+            java_cmd=""
+            java_cmd+="java -Xmx1024m -jar ${data_client_jar}"
+            java_cmd+=" -u ${g_user}"
+            java_cmd+=" -p ${g_password}"
+            java_cmd+=" -m PUT"
+            java_cmd+=" -r ${resource_uri}"
+			
+            log_Msg "Using java -Xmx1024m -jar ${data_client_jar} to PUT the resource: ${resource_uri}"
+            ${java_cmd}
+			
+        else
+            log_Msg "Did not attempt to put to resource: ${resource_url}"
+			
+        fi
+		
+    fi
 }
 
 # Invoke the main function to get things started
